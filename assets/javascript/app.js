591c40e9a2458aad0c83516e3f3372be47906e7b
AR123456 <coloradoz99@gmail.com>
2019-04-05 04:39:20 -0600

    objects constructors arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,24 @@
-console.log(document.cookie);
+var shoppingList = ["bread", "eggs", "milk"];
+showResult(shoppingList);
+showResult(shoppingList[0]);
+showResult(shoppingList[2]);
+
+function showResult(x) {
+  document.getElementById("myParagraph").innerHTML += "<br />" + x;
+}
+shoppingList[1] = "yogurt";
+showResult(shoppingList);
+shoppingList[2] = "orange";
+showResult(shoppingList);
+delete shoppingList[1];
+showResult(shoppingList);
+shoppingList.splice(1, 1);
+shoppingList.splice(1, 0, "banana", "apple");
+showResult(shoppingList);
+shoppingList.sort();
+showResult(shoppingList);
+shoppingList.reverse();
+showResult(shoppingList);
+var myString = "what is weather like?";
+var myArray = myString.split(" ", 2);
+showResult(myArray);
