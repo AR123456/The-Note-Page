6afb85e49d40cef4dc3fe71d976a7f3c05ca30a1
AR123456 <coloradoz99@gmail.com>
2019-05-06 04:42:33 -0600

    JS ajax bit coin fetch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,48 @@
-//git hub zen API
-var XHR = new XMLHttpRequest();
+var url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+fetch(url, {
+  method: "POST",
+  body: JSON.stringify({
+    name: "blue",
+    login: "bluecat"
+  })
+})
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data.bpi.EUR.rate);
+  });
 
-XHR.onreadystatechange = function() {
-  // checking out the ready state
-  //   console.log("Ready state is ...." + XHR.readyState);
-  if (XHR.readyState == 4) {
-    if (XHR.status == 200) {
-      console.log(XHR.responseText);
-    } else {
-      console.log("There was a problem");
-    }
-  }
-};
+// fetch(url, {
+//   method: "POST",
+//   body: "THIS IS THE BODY OF THE POST REQUEST "
+// })
+//   .then(function(response) {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log(data.bpi.EUR.rate);
+//   });
+// //////////////////////// XHR way //////////////////////
 
-// what type of request - retriving so get , then the url
-XHR.open("GET", "https://api.github.com/zen");
-//send it
-XHR.send();
+// var btn = document.querySelector("#btn");
+// var priceDisp = document.querySelector("#price");
+// //listen for clicks
+// btn.onclick = function() {
+//   var XHR = new XMLHttpRequest();
+//   XHR.onreadystatechange = function() {
+//     if (XHR.readyState == 4 && XHR.status == 200) {
+//       // var url = JSON.parse(XHR.responseText).message;
+//       // var price = (document.getElementById("#price").innerHTML =
+//       //   XHR.message.bpi.usd.rate_float);
+//       var data = JSON.parse(XHR.responseText);
+//       var price = data.bpi.USD.rate_float;
+//       priceDisp.innerText = price;
+//     }
+//   };
+
+//   XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
+//   XHR.send();
+// };
