a3bb4321a75c637c00ca1e5fc7e4a741a2c74eb0
AR123456 <coloradoz99@gmail.com>
2019-06-14 06:32:11 -0600

    JS and the keyword this

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,109 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var cw = canvas.width;
-var ch = canvas.height;
+// global context of this
+console.log(this); // window
 
-// define your barchart as a data-array of javascript objects
-var bars = [
-  { x: 50, y: ch, width: 20, height: 125, color: "green" },
-  { x: 80, y: ch, width: 20, height: 80, color: "red" },
-  { x: 110, y: ch, width: 20, height: 225, color: "blue" }
-];
-
-// draw the barchart based on the data array
-drawBars();
-
-function drawBars() {
-  ctx.textAlign = "center";
-  ctx.textBaseline = "bottom";
-  ctx.font = "14px verdana";
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, cw, ch);
-  for (var i = 0; i < bars.length; i++) {
-    var bar = bars[i];
-    ctx.fillStyle = bar.color;
-    ctx.fillRect(bar.x, bar.y - 20, bar.width, -bar.height);
-    ctx.fill();
-    ctx.strokeStyle = "lightgray";
-    ctx.strokeRect(bar.x, bar.y - 20, bar.width, -bar.height);
-    ctx.fillStyle = "white";
-    ctx.fillText(bar.height, bar.x + bar.width / 2, ch - 3);
-  }
+function whatIsThis() {
+  return this;
 }
+
+whatIsThis(); // window
+
+function variablesInThis() {
+  this.person = "Elie";
+}
+
+variablesInThis(); // the keyword this inside the function is the window
+
+console.log(person); // Elie
+
+////////////////strict mode
+("use strict");
+
+console.log(this); // window
+
+function whatIsThis() {
+  return this;
+}
+
+whatIsThis(); // undefined
+
+("use strict");
+
+function variablesInThis() {
+  // since we are in strict mode this is undefined
+  // so what happens if we add a property on undefined?
+  // let's see what happens when we call the function...
+  this.person = "Elie";
+}
+
+variablesInThis(); // TypeError, can't set person on undefined!
+
+//////////////////// Implicit / object
+// strict mode does NOT make a difference here
+
+var person = {
+  firstName: "Elie",
+  sayHi: function() {
+    return "Hi " + this.firstName;
+  },
+  determineContext: function() {
+    return this === person;
+  }
+};
+person.sayHi(); // "Hi Elie"
+person.determineContext(); // true
+
+////nested objects
+var person = {
+  firstName: "Colt",
+  sayHi: function() {
+    return "Hi " + this.firstName;
+  },
+  determineContext: function() {
+    return this === person;
+  },
+  dog: {
+    sayHello: function() {
+      return "Hello " + this.firstName;
+    },
+    determineContext: function() {
+      return this === person;
+    }
+  }
+};
+person.sayHi(); // "Hi Colt"
+person.determineContext(); // true
+// but what is the value of the keyword this right now?
+person.dog.sayHello(); // "Hello undefined"
+person.dog.determineContext(); // false
+
+/// explicit binding
+var person = {
+  firstName: "Colt",
+  sayHi: function() {
+    return "Hi " + this.firstName;
+  },
+  determineContext: function() {
+    return this === person;
+  },
+  dog: {
+    sayHello: function() {
+      return "Hello " + this.firstName;
+    },
+    determineContext: function() {
+      return this === person;
+    }
+  }
+};
+person.dog.sayHello.call(person); // "Hello Colt"
+person.dog.determineContext.call(person); // true
+
+// Using call worked!
+// Notice that we do NOT invoke sayHello or determineContext
+
+/////////////////// use case for call
+var divsArray = [].slice.call(divs);
+// you might also see this as Array.prototype.slice.call(divs)
+// they do the same thing
+divsArray.filter(function(val) {
+  return val.innerText === "Hello";
+});
