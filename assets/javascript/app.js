113ce30b64036b14d18fda2e05abac81540475d1
AR123456 <coloradoz99@gmail.com>
2019-06-17 07:35:34 -0600

    JS Canvas Basic Animation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,87 @@
-// // making sure window loads before the canvas starts
-// window.onload = function() {
-//   // a simple line
-//   var canvas = document.getElementById("hello-world-canvas");
-//   var context = canvas.getContext("2d");
-
-//   //zig
-//   context.beginPath(); //reset the context state
-//   context.strokeStyle = "red";
-//   context.moveTo(30, 30); // the start point of linex,y
-//   context.lineTo(80, 80); //end point of line c,y
-//   context.stroke(); //connect the points to draw the line
-//   //zag
-//   context.beginPath(); //reset the context state
-//   context.strokeStyle = "red";
-//   context.moveTo(80, 80); // the start point of linex,y
-//   context.lineTo(130, 30); //end point of line c,y
-//   context.stroke(); //connect the points to draw the line
-//   //zig
-//   context.beginPath(); //reset the context state
-//   context.strokeStyle = "red";
-//   context.moveTo(130, 30); // the start point of linex,y
-//   context.lineTo(180, 80); //end point of line c,y
-//   context.stroke(); //connect the points to draw the line
-//   //zag
-//   context.beginPath(); //reset the context state
-//   context.strokeStyle = "red";
-//   context.moveTo(180, 80); // the start point of linex,y
-//   context.lineTo(230, 30); //end point of line c,y
-//   context.stroke(); //connect the points to draw the line
-//   //zig
-
-//   context.beginPath(); //reset the context state
-//   context.strokeStyle = "red";
-//   context.moveTo(230, 30); // the start point of linex,y
-//   context.lineTo(280, 80); //end point of line c,y
-//   context.stroke(); //connect the points to draw the line
-// };
-// ******************************************** a better way
-// making sure window loads before the canvas starts
 window.onload = function() {
-  // a simple line
-  var canvas = document.getElementById("hello-world-canvas");
+  // Definitions
+  var canvas = document.getElementById("animations-canvas");
   var context = canvas.getContext("2d");
 
-  //zig
-  context.beginPath(); //reset the context state
-  context.strokeStyle = "red";
-  context.moveTo(30, 30); // the start point of linex,y
-  context.lineTo(80, 80); //end point of line c,y
-  context.lineTo(130, 30); //end point of line c,y
-  context.lineTo(180, 80); //end point of line c,y
-  context.lineTo(230, 30); //end point of line c,y
-  context.lineTo(280, 80); //end point of line c,y
-  context.stroke(); //connect the points to draw the line
+  var start = new Date();
+
+  window.requestAnimationFrame(drawRandomColoredRectangle);
+
+  function drawRandomColoredRectangle() {
+    var now = new Date();
+    if (now - start >= 1000) {
+      start = now;
+
+      // Clear canvas
+      //   context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // Random Colors
+      var color = createRandomRGBColor();
+      var fillOpacity = Math.random();
+      var fillColor =
+        "rgba(" +
+        color.r +
+        ", " +
+        color.g +
+        ", " +
+        color.b +
+        ", " +
+        fillOpacity +
+        ")";
+      var borderColor =
+        "rgba(" + color.r + ", " + color.g + ", " + color.b + ")";
+
+      var x = getRandomInt(0, canvas.width);
+      var y = getRandomInt(0, canvas.height);
+      var w = getRandomInt(0, canvas.width - x);
+      var h = getRandomInt(0, canvas.height - y);
+
+      // Draw Rectangle
+      context.beginPath();
+      context.fillStyle = fillColor;
+      context.strokeStyle = borderColor;
+      context.rect(x, y, w, h);
+      context.stroke();
+      context.fill();
+    }
+
+    // Animate
+    window.requestAnimationFrame(drawRandomColoredRectangle);
+  }
+
+  /**
+   *
+   * // Random Colors
+   * var color = createRandomRGBColor();
+   * var fillOpacity = Math.random();
+   * var fillColor = 'rgba('+ color.r + ', ' + color.g  + ', ' + color.b + ', ' + fillOpacity +')';
+   * var borderColor = 'rgba('+ color.r + ', ' + color.g  + ', ' + color.b + ')';
+   *
+   *
+   */
+  // function to generate random color
+  function createRandomRGBColor() {
+    var red = getRandomInt(0, 257);
+    var green = getRandomInt(0, 257);
+    var blue = getRandomInt(0, 257);
+    return { r: red, g: green, b: blue };
+  }
+  // function to generate random rectangles
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+  window.requestAnimationFrame = (function() {
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+      }
+    );
+  })();
 };
