38a2a0d6c6b24e7bb41135347a6ad1a1ebb13316
AR123456 <coloradoz99@gmail.com>
2019-06-18 05:52:41 -0600

    SEtting up Handle Bars

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-const express = require('express');
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
+// Handlebars Middleware
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
+
 // Index Route
-app.get('/', (req, res) => {
-  res.send('INDEX');
+app.get("/", (req, res) => {
+  //  this variable  title is geing passed into index.hanglebars inside of {{title}}
+  const title = "Welcome";
+  res.render("index", {
+    // title is being passed into view as the scond paramenter of an object
+    title: title
+  });
 });
 
 // About Route
-app.get('/about', (req, res) => {
-  res.send('ABOUT');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 const port = 5000;
 
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
