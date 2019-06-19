3588f748b475e39d43a3399fb2dc4806e2468eba
AR123456 <coloradoz99@gmail.com>
2019-06-19 05:12:49 -0600

    HTML5 Canvas complex shapes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,94 +1,63 @@
+// making sure window loads before the canvas starts
 window.onload = function() {
-  // Definitions
+  // a simple line
   var canvas = document.getElementById("lab-complex-shapes-canvas");
-  var context = canvas.getContext("2d");
+  var ctx = canvas.getContext("2d");
 
-  // Mouse Events
-  //  - mousedown / mouseup
-  //  - mouseover / mouseout
-  //  - mousemove
-  //  - click
-  //  - dblclick
+  /*
+  lineCap = butt|round|square
+  lineJoin = bevel|round|miter
+  shadowColor = color of shadow
+  shadowBlur = 	blur amount of shadows
+  shadowOffsetX	= horizontal distance of the shadow from the shape
+  shadowOffsetY	= vertical distance of the shadow from the shape
+  */
 
-  // canvas.addEventListener("mousedown", function(event) {
-  //   console.log("mousedown");
-  //   console.log(event);
-  // });
+  // First Z Object
+  ctx.beginPath();
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 15;
+  ctx.lineCap = "butt";
+  ctx.lineJoin = "miter";
+  ctx.shadowColor = "grey";
+  ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.shadowBlur = 10;
+  ctx.moveTo(60, 80);
+  ctx.lineTo(160, 80);
+  ctx.lineTo(80, 180);
+  ctx.lineTo(180, 180);
+  ctx.stroke();
 
-  // canvas.addEventListener("mouseup", function(event) {
-  //   console.log("mouseup");
-  //   console.log(event);
-  // });
+  // Second Z Object
+  ctx.beginPath();
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 15;
+  ctx.lineCap = "round";
+  ctx.lineJoin = "round";
+  ctx.shadowColor = "grey";
+  ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.shadowBlur = 10;
+  ctx.moveTo(340, 80);
+  ctx.lineTo(240, 80);
+  ctx.lineTo(340, 180);
+  ctx.lineTo(240, 180);
+  ctx.stroke();
 
-  // canvas.addEventListener("mouseover", function(event) {
-  //   console.log("mouseover");
-  //   console.log(event);
-  // });
-
-  // canvas.addEventListener("mouseout", function(event) {
-  //   console.log("mouseout");
-  //   console.log(event);
-  // });
-
-  // canvas.addEventListener("mousemove", function(event) {
-  //   console.log("mousemove");
-  //   console.log(event);
-  // });
-
-  canvas.addEventListener("click", function(event) {
-    console.log("click");
-    console.log(event);
-  });
-
-  // canvas.addEventListener("dblclick", function(event) {
-  //   console.log("dblclick");
-  //   console.log(event);
-  // });
+  // Third Z Object
+  ctx.beginPath();
+  ctx.strokeStyle = "green";
+  ctx.lineWidth = 15;
+  ctx.lineCap = "square";
+  ctx.lineJoin = "bevel";
+  ctx.shadowColor = "grey";
+  ctx.shadowOffsetX = 10;
+  ctx.shadowOffsetY = 10;
+  ctx.shadowBlur = 10;
+  ctx.moveTo(420, 80);
+  ctx.lineTo(520, 80);
+  ctx.lineTo(440, 180);
+  ctx.lineTo(540, 180);
+  ctx.stroke();
 };
-
-// could also listen to the window
-
-// window.onload = function() {
-
-//   // Mouse Events
-//   //  - mousedown / mouseup
-//   //  - mouseover / mouseout
-//   //  - mousemove
-//   //  - click
-//   //  - dblclick
-
-//   window.addEventListener("mousedown", function(event) {
-//     console.log("mousedown");
-//     console.log(event);
-//   });
-
-//   window.addEventListener("mouseup", function(event) {
-//     console.log("mouseup");
-//     console.log(event);
-//   });
-
-//   window.addEventListener("mouseover", function(event) {
-//     console.log("mouseover");
-//     console.log(event);
-//   });
-
-//   window.addEventListener("mouseout", function(event) {
-//     console.log("mouseout");
-//     console.log(event);
-//   });
-
-//   window.addEventListener("mousemove", function(event) {
-//     console.log("mousemove");
-//     console.log(event);
-//   });
-
-//   window.addEventListener("click", function(event) {
-//     console.log("click");
-//     console.log(event);
-//   });
-
-//  window.addEventListener("dblclick", function(event) {
-//     console.log("dblclick");
-//     console.log(event);
-//   });
-// };
