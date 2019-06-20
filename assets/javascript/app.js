66b785299f9bb1134e8fb4e643e1c5a76420dfd6
AR123456 <coloradoz99@gmail.com>
2019-06-20 07:16:47 -0600

    Canvas Curves and arcs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,34 @@
 // making sure window loads before the canvas starts
 window.onload = function() {
-  // a simple line
+  // paramiters
+
+  // arc(x.y, radius, startAngle, endAngle, counterClockWise )
+
   var canvas = document.getElementById("lab-complex-shapes-canvas");
   var ctx = canvas.getContext("2d");
+  var ctx = canvas.getContext("2d");
 
-  /*
-  lineCap = butt|round|square
-  lineJoin = bevel|round|miter
-  shadowColor = color of shadow
-  shadowBlur = 	blur amount of shadows
-  shadowOffsetX	= horizontal distance of the shadow from the shape
-  shadowOffsetY	= vertical distance of the shadow from the shape
-  */
+  //angles are in radians
+  var radian = Math.PI / 180;
 
-  // First Z Object
-  ctx.beginPath();
-  ctx.strokeStyle = "red";
-  ctx.lineWidth = 15;
-  ctx.lineCap = "butt";
-  ctx.lineJoin = "miter";
-  ctx.shadowColor = "grey";
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.shadowBlur = 10;
-  ctx.moveTo(60, 80);
-  ctx.lineTo(160, 80);
-  ctx.lineTo(80, 180);
-  ctx.lineTo(180, 180);
-  ctx.stroke();
-
-  // Second Z Object
+  // First Arc
   ctx.beginPath();
   ctx.strokeStyle = "blue";
-  ctx.lineWidth = 15;
-  ctx.lineCap = "round";
-  ctx.lineJoin = "round";
-  ctx.shadowColor = "grey";
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.shadowBlur = 10;
-  ctx.moveTo(340, 80);
-  ctx.lineTo(240, 80);
-  ctx.lineTo(340, 180);
-  ctx.lineTo(240, 180);
+  ctx.lineWidth = 10;
+  ctx.arc(100, 100, 50, 0 * radian, 180 * radian, false);
   ctx.stroke();
 
-  // Third Z Object
+  // Second Arc
+  ctx.beginPath();
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 10;
+  ctx.arc(300, 200, 50, 45 * radian, 130 * radian, false);
+  ctx.stroke();
+
+  // Third Arc
   ctx.beginPath();
   ctx.strokeStyle = "green";
-  ctx.lineWidth = 15;
-  ctx.lineCap = "square";
-  ctx.lineJoin = "bevel";
-  ctx.shadowColor = "grey";
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.shadowBlur = 10;
-  ctx.moveTo(420, 80);
-  ctx.lineTo(520, 80);
-  ctx.lineTo(440, 180);
-  ctx.lineTo(540, 180);
+  ctx.lineWidth = 10;
+  ctx.arc(300, 100, 50, 45 * radian, 130 * radian, true);
   ctx.stroke();
 };
