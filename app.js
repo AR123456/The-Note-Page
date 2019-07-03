f76b4dc95b4f25b242e720be52d92907cd4fbed9
AR123456 <coloradoz99@gmail.com>
2019-07-03 06:49:19 -0600

    Starter files for vidot project

--- app.js
+++ app.js
@@ -1,112 +1,30 @@
-const express = require("express");
-const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
-// Map global promise - get rid of warning
-mongoose.Promise = global.Promise;
-// Connect to mongoose
-mongoose
-  .connect("mongodb://localhost/vidjot-dev", {
-    useMongoClient: true
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(err => console.log(err));
-
-// Load Idea Model
-require("./models/Idea");
-const Idea = mongoose.model("ideas");
-
 // Handlebars Middleware
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Index Route
-app.get("/", (req, res) => {
-  const title = "Welcome";
-  res.render("index", {
+app.get('/', (req, res) => {
+  const title = 'Welcome';
+  res.render('index', {
     title: title
   });
 });
 
 // About Route
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-// Idea Index Page
-app.get("/ideas", (req, res) => {
-  Idea.find({})
-    .sort({ date: "desc" })
-    .then(ideas => {
-      res.render("ideas/index", {
-        ideas: ideas
-      });
-    });
-});
-
-//////// Add Idea Form
-app.get("/ideas/add", (req, res) => {
-  res.render("ideas/add");
-});
-
-//////////////////////// Edit Idea Form
-// pass along the id of the idea inot the get
-app.get("/ideas/edit/:id", (req, res) => {
-  // .findone jsut finds one not an array
-  Idea.findOne({
-    // use the request object for htis
-    _id: req.params.id
-    //in the promise grab the single idea
-  }).then(idea => {
-    // pass in the idea as object
-    res.render("ideas/edit", {
-      idea: idea
-    });
-  });
-});
-
-// Process Form
-app.post("/ideas", (req, res) => {
-  let errors = [];
-
-  if (!req.body.title) {
-    errors.push({ text: "Please add a title" });
-  }
-  if (!req.body.details) {
-    errors.push({ text: "Please add some details" });
-  }
-
-  if (errors.length > 0) {
-    res.render("ideas/add", {
-      errors: errors,
-      title: req.body.title,
-      details: req.body.details
-    });
-  } else {
-    const newUser = {
-      title: req.body.title,
-      details: req.body.details
-    };
-    new Idea(newUser).save().then(idea => {
-      res.redirect("/ideas");
-    });
-  }
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 const port = 5000;
 
-app.listen(port, () => {
+app.listen(port, () =>{
   console.log(`Server started on port ${port}`);
 });
