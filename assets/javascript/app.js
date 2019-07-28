b2f90792021be82e3d0973051c9ee4e44f50aada
AR123456 <coloradoz99@gmail.com>
2019-07-28 05:43:17 -0600

    HTML Canvas  bar graph

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,143 +1,32 @@
-// === Set up canvas === //
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var cw = canvas.width;
+var ch = canvas.height;
 
-var width = 750,
-  height = 400;
+// define your barchart as a data-array of javascript objects
+var bars = [
+  { x: 50, y: ch, width: 20, height: 125, color: "green" },
+  { x: 80, y: ch, width: 20, height: 80, color: "red" },
+  { x: 110, y: ch, width: 20, height: 225, color: "blue" }
+];
 
-var data = [];
-var value = 5000;
-var colourScale;
+// draw the barchart based on the data array
+drawBars();
 
-var canvas = d3
-  .select("#container")
-  .append("canvas")
-  .attr("width", width)
-  .attr("height", height);
-
-var context = canvas.node().getContext("2d");
-
-// === Load and prepare the data === //
-
-d3.range(value).forEach(function(el) {
-  data.push({ value: el });
-});
-
-console.log(data);
-
-// === Bind data to custom elements === //
-
-var customBase = document.createElement("custom");
-var custom = d3.select(customBase); // this is our svg replacement
-
-// settings for a grid with 40 cells in a row and 2x5 cells in a group
-var groupSpacing = 4;
-var cellSpacing = 2;
-var cellSize = Math.floor((width - 11 * groupSpacing) / 100) - cellSpacing;
-
-// === First call === //
-
-databind(data); // ...then update the databind function
-
-var t = d3.timer(function(elapsed) {
-  draw();
-  if (elapsed > 300) t.stop();
-}); // start a timer that runs the draw function for 500 ms (this needs to be higher than the transition in the databind function)
-
-// === Bind and draw functions === //
-
-function databind(data) {
-  colourScale = d3.scaleSequential(d3.interpolateSpectral).domain(
-    d3.extent(data, function(d) {
-      return d.value;
-    })
-  );
-
-  var join = custom.selectAll("custom.rect").data(data);
-
-  var enterSel = join
-    .enter()
-    .append("custom")
-    .attr("class", "rect")
-    .attr("x", function(d, i) {
-      var x0 = Math.floor(i / 100) % 10,
-        x1 = Math.floor(i % 10);
-      return groupSpacing * x0 + (cellSpacing + cellSize) * (x1 + x0 * 10);
-    })
-    .attr("y", function(d, i) {
-      var y0 = Math.floor(i / 1000),
-        y1 = Math.floor((i % 100) / 10);
-      return groupSpacing * y0 + (cellSpacing + cellSize) * (y1 + y0 * 10);
-    })
-    .attr("width", 0)
-    .attr("height", 0);
-
-  join
-    .merge(enterSel)
-    .transition()
-    .attr("width", cellSize)
-    .attr("height", cellSize)
-    .attr("fillStyle", function(d) {
-      return colourScale(d.value);
-    });
-
-  var exitSel = join
-    .exit()
-    .transition()
-    .attr("width", 0)
-    .attr("height", 0)
-    .remove();
-} // databind()
-
-// === Draw canvas === //
-
-function draw() {
-  // clear canvas
-
-  context.fillStyle = "#fff";
-  context.fillRect(0, 0, width, height);
-
-  // draw each individual custom element with their properties
-
-  var elements = custom.selectAll("custom.rect"); // this is the same as the join variable, but used here to draw
-
-  elements.each(function(d, i) {
-    // for each virtual/custom element...
-
-    var node = d3.select(this);
-    context.fillStyle = node.attr("fillStyle");
-    context.fillRect(
-      node.attr("x"),
-      node.attr("y"),
-      node.attr("width"),
-      node.attr("height")
-    );
-  });
-} // draw()
-
-// === Listeners/handlers === //
-
-d3.select("#text-input").on("keydown", function() {
-  if (d3.event.keyCode === 13) {
-    d3.select("#alert").html("");
-
-    if (+this.value < 1 || +this.value > 10000) {
-      d3.select("#text-explain").classed("alert", true);
-
-      return;
-    } else {
-      d3.select("#text-explain").classed("alert", false);
-
-      data = [];
-
-      d3.range(+this.value).forEach(function(el) {
-        data.push({ value: el });
-      });
-
-      databind(data);
-
-      var t = d3.timer(function(elapsed) {
-        draw();
-        if (elapsed > 500) t.stop();
-      }); // start a timer that runs the draw function for 500 ms (this needs to be higher than the transition in the databind function)
-    } // value test
-  } // keyCode 13 === return
-}); // text input listener/handler
+function drawBars() {
+  ctx.textAlign = "center";
+  ctx.textBaseline = "bottom";
+  ctx.font = "14px verdana";
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, cw, ch);
+  for (var i = 0; i < bars.length; i++) {
+    var bar = bars[i];
+    ctx.fillStyle = bar.color;
+    ctx.fillRect(bar.x, bar.y - 20, bar.width, -bar.height);
+    ctx.fill();
+    ctx.strokeStyle = "lightgray";
+    ctx.strokeRect(bar.x, bar.y - 20, bar.width, -bar.height);
+    ctx.fillStyle = "white";
+    ctx.fillText(bar.height, bar.x + bar.width / 2, ch - 3);
+  }
+}
