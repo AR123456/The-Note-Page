ff40c5209ff12e2ba05dff364cec5af2bf36a790
AR123456 <coloradoz99@gmail.com>
2019-10-08 05:04:24 -0600

    prototype model

--- sandbox.js
+++ sandbox.js
@@ -1,28 +1,26 @@
-// constructor functions
-// class User {
-//   constructor(username){
-//     this.username = username;
-//   }
-// }
-// JS classes under the hood are a prototype model
-// this is the code that lives under the hood when we use class above.
-// this is the old way of writing that does the same thing
 function User(username, email) {
   this.username = username;
   this.email = email;
-  // it is better to do this using prototype 
-  this.login = function() {
-    console.log(`${this.username} has logged in`);
-  };
 }
+// many methods avalbile via prototype- JS proxies up onto the root level of the object
+// store the method onthe prototype
+// the class does this automaticaly  but becuae in this example we are not using class we need to do that
+//store the methods on the prototype - one location , can use prototypical inheritnace
+
+// adding methods to prototype with .  set it equal to a funcition and do stuff inside
+User.prototype.login = function() {
+  console.log(`${this.username} has logged in`);
+  return this; // return this so we can methond chaing
+};
+// can add more that one
+User.prototype.logout = function() {
+  console.log(`${this.username} has logged out`);
+  return this; // do this so we can method chain
+};
 
 const userOne = new User("ryu", "ryu@thenetninja.co.uk");
 const userTwo = new User("chun-li", "chun.li@thenetninja.co.uk");
 
-console.log(userOne, userTwo);
-userOne.login();
-
-// the 'new' keyword
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object
-// 3 - it calls the constructor function to 'build' the object
+console.log(userOne);
+// can chain them
+userOne.login().logout();
