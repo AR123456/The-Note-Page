31003bf2555ebfaaf746db9b664037da595ad403
AR123456 <coloradoz99@gmail.com>
2019-10-08 04:59:02 -0600

    object literals

---
+++ sandbox.js
@@ -0,0 +1,30 @@
+const userOne = {
+  username: "ryu",
+  email: "ryu@thenetninja.co.uk",
+  login() {
+    console.log("the user logged in");
+  },
+  logout() {
+    console.log("the user logged out");
+  }
+};
+
+console.log(userOne.email, userOne.username);
+userOne.login();
+
+const userTwo = {
+  username: "chun-li",
+  email: "chun.li@thenetninja.co.uk",
+  login() {
+    console.log("the user logged in");
+  },
+  logout() {
+    console.log("the user logged out");
+  }
+};
+
+ 
+
+ 
+
+ 
