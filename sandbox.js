9d012f09805542fba720705650b1e3fdc3909ed1
AR123456 <coloradoz99@gmail.com>
2019-10-08 05:02:13 -0600

    chaining subclasses

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,51 @@
 class User {
-  //making the constructor function to construct the object and set the properties on it
-  // this is the same shorthand syntax for a function that we used inside objects it must have the constructor in its name
-  // must use constructor name
   constructor(username, email) {
-    // using the same shorthand syntax that we used in objects
-
-    // inside the constructor set up properties on the object.
-    // have access to all the stuff riding on "this" that is coming from the const set to new User
-
-    // this.username = 'mario';
+    //use the constructor to set up the properties
     this.username = username;
     this.email = email;
+    this.score = 0;
+  } //- inside a class dont comma separate different methods
+  // this is a regular function using shorthand notation because arrow fundtions do not bind a value to the keyword "this" when they are called
+  // if we used an arrow function here "this" inside the function itself it would refer to the window object
+  // by using a regular function the "this" keywod will be the object instance, that is the object we create when we use new user
+  login() {
+    console.log(`${this.username} just logged in`);
+    // if you do not expicitly return JS will return undefined
+    return this; // this refers to the object instance itself- do this if you want to chain
+  }
+  logout() {
+    console.log(`${this.username} just logged out`);
+    return this;
+  }
+  incScore() {
+    this.score += 1;
+    console.log(`${this.username} has a score of ${this.score}`);
+    return this;
   }
 }
-// this is calling the constructor funcion that is inside the class to set the properties on the new user object
-// pass in paramiters here so that they can be used as argumetns in the constructor
-
+// class inheritance - sub classes > the inherit functionality from another class but have their own additional properties or methods too. then extend another class
+// this Admin class is inheriting all of the properties and methods of User  pluss have the deleteUser method
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter(u => u.username !== user.username);
+    return this; // allow method chaining
+  }
+}
+// the new user objects created here have access to all fo the functions inside the class
 const userOne = new User("luigi", "luigi@thenetninja.co.uk");
 const userTwo = new User("mario", "mario@thenetninja.co.uk");
+const userThree = new Admin("shaun", "shaun@thenetninja.co.uk");
 
-console.log(userOne, userTwo);
+console.log(userOne, userThree);
 
-// the 'new' keyword - does three things
-// 1 - it creates a new empty object {}
-// 2 - it binds the value of 'this' to the new empty object inside the class
-// 3 - it calls the constructor function to 'build' the object inside the class
+let users = [userOne, userTwo, userThree];
+console.log(users);
+// the filter method above filters out the deleted user
+userThree.deleteUser(userTwo);
+console.log(users);
+
+userOne
+  .login()
+  .incScore()
+  .incScore()
+  .logout();
