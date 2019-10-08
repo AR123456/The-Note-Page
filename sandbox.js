86710a8419f97524594fdc6fadd3ae26a4f75ea9
AR123456 <coloradoz99@gmail.com>
2019-10-08 05:01:10 -0600

    classes

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,5 @@
-const userOne = {
-  username: "ryu",
-  email: "ryu@thenetninja.co.uk",
-  login() {
-    console.log("the user logged in");
-  },
-  logout() {
-    console.log("the user logged out");
-  }
-};
+// class is syntastic sugar to make working with ojects easier
 
-console.log(userOne.email, userOne.username);
-userOne.login();
-
-const userTwo = {
-  username: "chun-li",
-  email: "chun.li@thenetninja.co.uk",
-  login() {
-    console.log("the user logged in");
-  },
-  logout() {
-    console.log("the user logged out");
-  }
-};
-
- 
-
- 
-
- 
+class User {
+  // classes are like a blueprint
+}
