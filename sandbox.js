9b9a2f2143e273f0484fb8d6aaaa8fe2e53bb500
AR123456 <coloradoz99@gmail.com>
2019-10-11 05:58:17 -0600

    real time listoner

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,40 @@
 const list = document.querySelector('ul');
 const form = document.querySelector('form');
 
-const addRecipe = (recipe) => {
+const addRecipe = (recipe, id) => {
   let time = recipe.created_at.toDate();
   let html = `
-    <li>
+    <li data-id="${id}">
       <div>${recipe.title}</div>
       <div><small>${time}</small></div>
+      <button class="btn btn-danger btn-sm my-2">delete</button>
     </li>
   `;
 
   list.innerHTML += html;
 };
 
-// get documents
-db.collection('recipes').get().then(snapshot => {
-  snapshot.docs.forEach(doc => {
-    addRecipe(doc.data());
+const deleteRecipe = (id) => {
+  const recipes = document.querySelectorAll('li');
+  recipes.forEach(recipe => {
+    if(recipe.getAttribute('data-id') === id){
+      recipe.remove();
+    }
   });
-}).catch(err => {
-  console.log(err);
+};
+
+// real-time listener
+db.collection('recipes').onSnapshot(snapshot => {
+  console.log(snapshot.docChanges());
+  snapshot.docChanges().forEach(change => {
+    const doc = change.doc;
+    if(change.type === 'added'){
+      // console.log(doc);
+      addRecipe(doc.data(), doc.id)
+    } else if (change.type === 'removed'){
+      deleteRecipe(doc.id);
+    }
+  });
 });
 
 // save documents
@@ -33,8 +48,19 @@ form.addEventListener('submit', e => {
   };
 
   db.collection('recipes').add(recipe).then(() => {
-    console.log('recipe added');
+    //console.log('recipe added');
+    form.reset();
   }).catch(err => {
     console.log(err);
   });
+});
+
+// deleting data
+list.addEventListener('click', e => {
+  if(e.target.tagName === 'BUTTON'){
+    const id = e.target.parentElement.getAttribute('data-id');
+    db.collection('recipes').doc(id).delete().then(() => {
+      // console.log('recipe deleted');
+    });
+  }
 });
