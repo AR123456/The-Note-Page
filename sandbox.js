a66ed72cc80a5776ab51e8d4bdba35a29d39d533
AR123456 <coloradoz99@gmail.com>
2019-10-11 05:56:34 -0600

    del doc

--- sandbox.js
+++ sandbox.js
@@ -1,56 +1,29 @@
-const list = document.querySelector("ul");
-const form = document.querySelector("form");
-const button = document.querySelectorAll("button");
+const list = document.querySelector('ul');
+const form = document.querySelector('form');
 
-const addRecipe = (recipe, id) => {
+const addRecipe = (recipe) => {
   let time = recipe.created_at.toDate();
-  let html = `<li data-id="${id}">
-  <div>${recipe.title}</div>
-  <div><small>${time}</small></div>
-  <button class="btn btn-danger btn-sm my-2">Delete</button>
-  </li>`;
+  let html = `
+    <li>
+      <div>${recipe.title}</div>
+      <div><small>${time}</small></div>
+    </li>
+  `;
+
   list.innerHTML += html;
 };
-// delete from UI //
-const deleteRecipe = id => {
-  const recipes = document.querySelectorAll("li");
-  recipes.forEach(recipe => {
-    if (recipe.getAttribute("data-id") === id) {
-      recipe.remove();
-    }
+
+// get documents
+db.collection('recipes').get().then(snapshot => {
+  snapshot.docs.forEach(doc => {
+    addRecipe(doc.data());
   });
-};
-// det documents
-// real time event delegation - set up real time listioner
-const unsub = db.collection("recipes").onSnapshot(snapshot => {
-  console.log(snapshot.docChanges());
-  snapshot.docChanges().forEach(change => {
-    const doc = change.doc;
-    if (change.type === "added") {
-      // console.log(doc);
-      addRecipe(doc.data(), doc.id);
-    } else if (change.type === "removed") {
-      deleteRecipe(doc.id);
-    }
-  });
+}).catch(err => {
+  console.log(err);
 });
 
-// get documents  - the non event listen way
-// db.collection("recipes")
-//   .get()
-//   .then(snapshot => {
-//     //when we have the data
-//     snapshot.docs.forEach(doc => {
-//       // get the id property , add it as a second argument
-//       addRecipe(doc.data(), doc.id);
-//     });
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-// add documents
-form.addEventListener("submit", e => {
+// save documents
+form.addEventListener('submit', e => {
   e.preventDefault();
 
   const now = new Date();
@@ -58,29 +31,10 @@ form.addEventListener("submit", e => {
     title: form.recipe.value,
     created_at: firebase.firestore.Timestamp.fromDate(now)
   };
-  db.collection("recipes")
-    .add(recipe)
-    .then(() => {
-      console.log("recipe added");
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-// delete documents with event delegation
-list.addEventListener("click", e => {
-  if (e.target.tagName === "BUTTON") {
-    const id = e.target.parentElement.getAttribute("data-id");
-    db.collection("recipes")
-      .doc(id)
-      .delete()
-      .then(() => {
-        // console.log("recipe deleted");
-      });
-  }
-});
-// unsub from database changes
-button.addEventListener("click", () => {
-  unsub();
-  console.log("unsub from collection changes");
+
+  db.collection('recipes').add(recipe).then(() => {
+    console.log('recipe added');
+  }).catch(err => {
+    console.log(err);
+  });
 });
