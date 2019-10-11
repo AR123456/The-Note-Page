3440765f0292562e17be78f948ee539de751a272
AR123456 <coloradoz99@gmail.com>
2019-10-11 05:54:54 -0600

    firebase

--- sandbox.js
+++ sandbox.js
@@ -1,36 +1,86 @@
-// user constructor
-function User(username, email) {
-  this.username = username;
-  this.email = email;
-}
+const list = document.querySelector("ul");
+const form = document.querySelector("form");
+const button = document.querySelectorAll("button");
 
-User.prototype.login = function() {
-  console.log(`${this.username} has logged in`);
-  return this;
+const addRecipe = (recipe, id) => {
+  let time = recipe.created_at.toDate();
+  let html = `<li data-id="${id}">
+  <div>${recipe.title}</div>
+  <div><small>${time}</small></div>
+  <button class="btn btn-danger btn-sm my-2">Delete</button>
+  </li>`;
+  list.innerHTML += html;
 };
-
-User.prototype.logout = function() {
-  console.log(`${this.username} has logged out`);
-  return this;
+// delete from UI //
+const deleteRecipe = id => {
+  const recipes = document.querySelectorAll("li");
+  recipes.forEach(recipe => {
+    if (recipe.getAttribute("data-id") === id) {
+      recipe.remove();
+    }
+  });
 };
+// det documents
+// real time event delegation - set up real time listioner
+const unsub = db.collection("recipes").onSnapshot(snapshot => {
+  console.log(snapshot.docChanges());
+  snapshot.docChanges().forEach(change => {
+    const doc = change.doc;
+    if (change.type === "added") {
+      // console.log(doc);
+      addRecipe(doc.data(), doc.id);
+    } else if (change.type === "removed") {
+      deleteRecipe(doc.id);
+    }
+  });
+});
 
-// admin - create admin user type
-function Admin(username, email) {
-  // the .call method calls the user constructor
-  //argument one is the context of what the this keyword will be equal to inside the user construtor funcion(new Admin )
-  //argument two and beyond are the acutal parameters that we need to take in
-  User.call(this, username, email);
-}
+// get documents  - the non event listen way
+// db.collection("recipes")
+//   .get()
+//   .then(snapshot => {
+//     //when we have the data
+//     snapshot.docs.forEach(doc => {
+//       // get the id property , add it as a second argument
+//       addRecipe(doc.data(), doc.id);
+//     });
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
-Admin.prototype = Object.create(User.prototype);
+// add documents
+form.addEventListener("submit", e => {
+  e.preventDefault();
 
-Admin.prototype.deleteUser = function(user) {
-  // delete the user
-};
-
-const userOne = new User("ryu", "ryu@thenetninja.co.uk");
-const userTwo = new User("chun-li", "chun.li@thenetninja.co.uk");
-const userThree = new Admin("shaun", "shaun@thenetninja.co.uk");
-
-console.log(userThree);
-// prototype chain , nested levels
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now)
+  };
+  db.collection("recipes")
+    .add(recipe)
+    .then(() => {
+      console.log("recipe added");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+// delete documents with event delegation
+list.addEventListener("click", e => {
+  if (e.target.tagName === "BUTTON") {
+    const id = e.target.parentElement.getAttribute("data-id");
+    db.collection("recipes")
+      .doc(id)
+      .delete()
+      .then(() => {
+        // console.log("recipe deleted");
+      });
+  }
+});
+// unsub from database changes
+button.addEventListener("click", () => {
+  unsub();
+  console.log("unsub from collection changes");
+});
