8123149185eb9d333ff2e78918ebae10431eb32a
AR123456 <coloradoz99@gmail.com>
2019-10-15 04:39:51 -0600

    symbols

--- sandbox.js
+++ sandbox.js
@@ -1,39 +1,22 @@
-// sets - new data stuctue in the object category
-//reference type
-// allows storage of unique values
-// in a given set a certain value can olny be present once
-// have a different set of methods and propeties than arrays
-const namesArray = ["ryu", "chun-li", "ryu", "shaun"];
-console.log(namesArray);
+// symbols- new primitive data type
+// no 2 symbols can ever be equal to one another
+// Symbols can be used as keys or property names in objecs
+//
 
-// const namesSet = new Set(['ryu', 'chun-li', 'ryu', 'shaun']);
-const namesSet = new Set(namesArray);
-// duplicates removed now
-console.log(namesSet);
-//// snow spreading de duplicated into a new array
-const uniqueNames = [...namesSet];
-////// this is the syntax for doing this on one line, make the set and and spread it into a new arrray
-// const uniqueNames = [...new Set(namesArray)];
-// console.log(uniqueNames);
+const symbolOne = Symbol("a generic name");
+const symbolTwo = Symbol("a generic name");
 
-const ages = new Set();
-ages.add(20);
-ages.add(25).add(30);
-ages.add(25);
-ages.delete(30);
+console.log(symbolOne, typeof symbolOne);
+console.log(symbolOne === symbolTwo);
+// createing a ninja object, makes all of the keys unique even if they apper the same
+const ninja = {};
 
-console.log(ages, ages.size);
-console.log(ages.has(30), ages.has(20));
+ninja.age = 30;
+ninja["belt"] = "orange";
+ninja["belt"] = "black";
 
-ages.clear();
-console.log(ages);
+ninja[symbolOne] = "ryu";
+ninja[symbolTwo] = "shaun";
 
-const ninjas = new Set([
-  { name: "shaun", age: 30 },
-  { name: "crystal", age: 29 },
-  { name: "chun-li", age: 32 }
-]);
-// can use for each on a set
-ninjas.forEach(ninja => {
-  console.log(ninja.name, ninja.age);
-});
+console.log(ninja);
+console.log(ninja[symbolOne], ninja[symbolTwo]);
