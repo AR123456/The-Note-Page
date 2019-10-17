13fc343de198985393e4c21fa2080952ac693e84
AR123456 <coloradoz99@gmail.com>
2019-10-17 05:01:56 -0600

    default exprorts

--- src/dom.js
+++ src/dom.js
@@ -1,19 +1,19 @@
-console.log("dom.js file");
+console.log('dom.js file');
 
 // dom queries
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 
 const styleBody = () => {
-  body.style.background = "peachpuff";
+  body.style.background = 'peachpuff';
 };
 
-const addTitle = text => {
-  const title = document.createElement("h1");
+const addTitle = (text) => {
+  const title = document.createElement('h1');
   title.textContent = text;
   body.appendChild(title);
 };
 
-const contact = "mario@thenetninja.co.uk";
+const contact = 'mario@thenetninja.co.uk';
 
 // styleBody();
 // addTitle('hello, world from dom.js');
