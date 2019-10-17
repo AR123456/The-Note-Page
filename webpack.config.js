40969736c3b8aec4230fb36f0c3788390233d323
AR123456 <coloradoz99@gmail.com>
2019-10-17 05:01:16 -0600

    intor ot modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,9 @@
-// path is a node module
-const path = require("path");
-// the options object - not on the browser runs on computer via node - what is output will be run in the browser
+const path = require('path');
+
 module.exports = {
-  // relative path to the entry file the input
-  entry: "./src/index.js",
-  // specify the absolute URL so use the node dirname property to get the absolute path no matter where it is
+  entry: './src/index.js',
   output: {
-    // node path.resolve method takes in 2 params the dist foulder
-    path: path.resolve(__dirname, "dist"),
-    // file name
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist/assets'),
+    filename: 'bundle.js'
   }
 };
