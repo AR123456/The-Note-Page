d54ceeb24a13f7168167aef9ce209902e0f4a713
AR123456 <coloradoz99@gmail.com>
2019-10-26 06:24:15 -0600

    console log gems

---
+++ app.js
@@ -0,0 +1,34 @@
+// Log to console
+console.log("Hello World");
+// numbers are blue in chrone console
+console.log(123);
+console.log(true);
+var greeting = "Hello";
+console.log(greeting);
+// can console.log an object
+console.log([1, 2, 3, 4]);
+// can console.log an object
+console.log({ a: 1, b: 2 });
+// console log a table
+console.table({ a: 1, b: 2 });
+
+console.error("This is some error");
+// this clears the console
+console.clear();
+// log a warning
+console.warn("This is a warning");
+// the timer start
+console.time("Hello");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+//timer end - to determine how long code has taken
+console.timeEnd("Hello");
+/*
+  multi
+  line
+  comments
+*/
