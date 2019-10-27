27951b2501c8fefb6ff11903b2e78045908c45a1
AR123456 <coloradoz99@gmail.com>
2019-10-27 06:20:02 -0600

    js data types

--- app.js
+++ app.js
@@ -1,53 +1,35 @@
-// var, let, const
+// PRIMITIVE data types
 
-var name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
+// String
+const name = "John Doe";
+console.log(typeof name);
+// Number
+const age = 45;
+console.log(typeof age);
+// Boolean
+const hasKids = true;
+console.log(typeof hasKids);
+// Null
+const car = null;
+console.log(typeof car);
+// Undefined
+let test;
+console.log(typeof test);
+// Symbol
+const sym = Symbol();
+console.log(typeof sym);
 
-// // // Init var
-var greeting;
-// console.log(greeting);
-greeting = 'Hello';
-console.log(greeting);
-
-///// // letters, numbers, _, $,   the _ is generaly used for private variables 
-// /////// Can not start with number
-
-// // Multi word vars
-var firstName = 'John'; // Camel case- best practice for variables 
-var first_name = 'Sara'; // Underscore
-var FirstName = 'Tom'; // Pascal case- class and constructors use this convention 
-var firstname;
-
-// LET
-let name;
-name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
-
-
-// CONST --- use const unless it is an itorator or loop, is a value that is changing or need to initialize it
-// cannot be reasinged as a primitive value 
-const name = 'John';
-console.log(name);
-// Can not reassign
-name = 'Sara';
-// Have to assign a value
-const greeting;
-
-const person = {
-  name: 'John',
-  age: 30
-}
-
-person.name = 'Sara';
-person.age = 32;
-
-console.log(person);
-
-const numbers = [1,2,3,4,5];
-numbers.push(6);
-
-console.log(numbers);
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ["movies", "music"];
+console.log(typeof hobbies);
+// Object literal
+const address = {
+  city: "Boston",
+  state: "MA"
+};
+console.log(typeof address);
+const today = new Date();
+console.log(typeof today);
+console.log(today);
+console.log(typeof today);
