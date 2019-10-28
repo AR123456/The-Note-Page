08eaea17eb07c95a858db25fcde53859efdddea9
AR123456 <coloradoz99@gmail.com>
2019-10-28 05:55:31 -0600

    type conversion

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
-const num1 = 100;
-const num2 = 50;
+// type conversion  change a variabels type
+
 let val;
 
-// Simple math with numbers
-val = num1 + num2;
-val = num1 * num2;
-val = num1 - num2;
-val = num1 / num2;
-val = num1 % num2;
+// Number to string
+val = String(555);
+val = String(4 + 4);
+console.log(typeof val);
+// Bool to string
+val = String(true);
+console.log(typeof val);
+// Date to string
+val = String(new Date());
+console.log(typeof val);
+// Array to string
+val = String([1, 2, 3, 4]);
+console.log(typeof val);
 
-// Math Object
-val = Math.PI;
-val = Math.E; // eulesr number
-val = Math.round(2.4); //rounding
-val = Math.ceil(2.4); // roundup
-val = Math.floor(2.8); // round down
-val = Math.sqrt(64); // square root
-val = Math.abs(-3); // absolute number
-val = Math.pow(8, 2); // power of something
-val = Math.min(2, 33, 4, 1, 55, 6, 3, -2); // min
-val = Math.max(2, 33, 4, 1, 55, 6, 3, -2); //max
-val = Math.random(); // get a random number (decimle )
+// toString()
+val = (5).toString();
 
-val = Math.floor(Math.random() * 20 + 1); // way to get random whole number between 1 and 20
+val = true.toString();
 
+// String to number
+val = Number("5");
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number("hello");
+val = Number([1, 2, 3]);
+
+val = parseInt("100.30");
+val = parseFloat("100.30");
+
+// Output
 console.log(val);
+console.log(typeof val);
+console.log(val.length);
+console.log(val.toFixed(2));
+// type coersion - js turns val2 to string and concatonates
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
