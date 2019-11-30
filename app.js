71a3a31da45610e254a326d91de166833f740a54
AR123456 <coloradoz99@gmail.com>
2019-11-30 06:29:54 -0700

    javascript sets

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-// Init weather object
-const weather = new Weather('Boston', 'MA');
+// SETS - Store unique values of any type
 
-// Get weather on DOM load
-document.addEventListener('DOMContentLoaded', getWeather);
+const set1 = new Set();
 
-// weather.changeLocation('Miami', 'FL');
+// Add values to set
+set1.add(100);
+set1.add('A string');
+set1.add({name: 'John'});
+set1.add(true);
+set1.add(100);
 
-function getWeather(){
-  weather.getWeather()
-    .then(results => {
-      console.log(results);
-    })
-    .catch(err => console.log(err));
-}
+// const set2 = new Set([1, true, 'string']);
+// console.log(set2);
+
+// console.log(set1);
+
+// Get count
+// console.log(set1.size);
+
+// Check for values
+// console.log(set1.has(100));
+// console.log(set1.has(50 + 50));
+// console.log(set1.has({name: 'John'}));
+
+// Delete from set
+// set1.delete(100);
+
+// console.log(set1);
+
+// ITERATING THROUGH SETS
+
+// For..of 
+// for(let item of set1) {
+//   console.log(item);
+// }
+
+// ForEach Loop
+// set1.forEach((value) => {
+//   console.log(value);
+// });
+
+// CONVERT SET TO ARRAY
+const setArr = Array.from(set1);
+console.log(setArr);
