85a71bed3849dd160b5df49b17a928896f53e2de
AR123456 <coloradoz99@gmail.com>
2019-12-02 07:07:18 -0700

    getInstance

--- app.js
+++ app.js
@@ -1,56 +1,24 @@
-// Basic structure
+const Singleton = (function() {
+  let instance;
 
-// (function() {
-//   // Declare private vars and functions
-
-//   return {
-//     // Declare public var and functions
-//   }
-// })();
-
-// STANDARD MODULE PATTERN
-// const UICtrl = (function() {
-//   let text = 'Hello World';
-
-//   const changeText = function() {
-//     const element = document.querySelector('h1');
-//     element.textContent = text;
-//   }
-
-//   return {
-//     callChangeText: function() {
-//       changeText();
-//       // console.log(text);
-//     }
-//   }
-// })();
-
-// UICtrl.callChangeText();
-// // UICtrl.changeText();
-
-// console.log(UICtrl.text);
-
-// REVEALING MODULE PATTERN
-const ItemCtrl = (function() {
-  let data = [];
-
-  function add(item) {
-    data.push(item);
-    console.log('Item Added....');
-  }
-
-  function get(id) {
-    return data.find(item => {
-      return item.id === id;
-    });
+  function createInstance() {
+    const object = new Object({name:'Brad'});
+    return object;
   }
 
   return {
-    add: add,
-    // get: get
+    getInstance: function() {
+      if(!instance){
+        instance = createInstance();
+      }
+      return instance;
+    }
   }
 })();
 
-ItemCtrl.add({id: 1, name: 'John'});
-ItemCtrl.add({id: 2, name: 'Mark'});
-console.log(ItemCtrl.get(2));
+const instanceA = Singleton.getInstance();
+const instanceB = Singleton.getInstance();
+
+console.log(instanceA === instanceB);
+
+// console.log(instanceA);
