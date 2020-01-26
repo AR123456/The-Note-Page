ebde8a0686dd5345f5f8d52295a10d360c3fad43
AR123456 <coloradoz99@gmail.com>
2020-01-26 10:29:42 -0700

    base 64 encoding

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
- 
 document.querySelector("form").addEventListener("submit", event => {
   event.preventDefault();
-  const input = document.querySelector("input");
-  console.log(input.value);
+  const input = document.querySelector("#message-input");
+
+  // btoa encodes
+  const codedInput = btoa(input.value);
+  document.querySelector("#link-input").value = codedInput;
+  console.log(codedInput);
+  // atob() decodes
+  const decodeInput = atob(codedInput);
+  console.log(decodeInput);
 });
