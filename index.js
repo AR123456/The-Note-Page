c9f96cf97e555771a02c4ef97f3c36a0d8a732e7
AR123456 <coloradoz99@gmail.com>
2020-01-26 10:30:20 -0700

    generate url

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ document.querySelector("form").addEventListener("submit", event => {
   const input = document.querySelector("#message-input");
 
   // btoa encodes
-  const codedInput = btoa(input.value);
-  document.querySelector("#link-input").value = codedInput;
-  console.log(codedInput);
-  // atob() decodes
-  const decodeInput = atob(codedInput);
-  console.log(decodeInput);
+  const encrypted = btoa(input.value);
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
 });
