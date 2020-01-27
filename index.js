1b7e3617583aaa1950a853189e155acae72effae
AR123456 <coloradoz99@gmail.com>
2020-01-27 06:01:50 -0700

    toggle element visibility

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 document.querySelector("form").addEventListener("submit", event => {
   event.preventDefault();
   const input = document.querySelector("#message-input");
-
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
   // btoa encodes
   const encrypted = btoa(input.value);
   const linkInput = document.querySelector("#link-input");
