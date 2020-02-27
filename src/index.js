5524d7ae59762b0543311eb4c7c827173fbd943f
AR123456 <coloradoz99@gmail.com>
2020-02-27 06:22:12 -0700

    working with react components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-  <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-  </ul>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
