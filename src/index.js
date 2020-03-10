0fc246f63d80af390497f85f1cc2ce16b41de619
AR123456 <coloradoz99@gmail.com>
2020-03-10 06:03:33 -0600

    react map filter reduce

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+var numbers = [3, 56, 2, 48, 5];
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//Find - find the first item that matches from an array.
+const foundNum = numbers.find(function(num) {
+  return num > 10;
+});
+console.log(foundNum);
+//FindIndex - find the index of the first item that matches.
+const foundIndex = numbers.findIndex(function(num) {
+  return num > 10;
+});
+console.log(foundIndex);
