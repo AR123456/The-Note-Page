493f14d9e0ea26f75b923cf61bd3a8a0af10dbae
AR123456 <coloradoz99@gmail.com>
2020-03-12 04:57:50 -0600

    intro to state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import Form from "./Form";
 
-var userIsRegistered = false;
+var isDone = false;
+
+function strike() {
+  isDone = true;
+}
+
+function unStrike() {
+  isDone = false;
+}
 
 function App() {
   return (
-    <div className="container">
-      <Form isRegistered={userIsRegistered} />
+    <div>
+      <p style={isDone ? { textDecoration: "line-through" } : null}>Buy milk</p>
+      <button onClick={strike}>Change to strike through</button>
+      <button onClick={unStrike}> Change back</button>
     </div>
   );
 }
