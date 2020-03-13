9d4b5cdb8fea5712f6b089bdd3aa443858fee067
AR123456 <coloradoz99@gmail.com>
2020-03-13 07:17:25 -0600

    hook practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function increase() {
-    setCount(count + 1);
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
+  console.log(now);
+  function updateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
   }
 
-  function decrease() {
-    setCount(count - 1);
-  }
+  setInterval(updateTime, 1000);
 
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={decrease}>-</button>
-      <button onClick={increase}>+</button>
+      <h1>{time}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
