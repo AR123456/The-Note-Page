1d5b1c901153b0ef6b26c1764e3e8150ca26808f
AR123456 <coloradoz99@gmail.com>
2020-03-14 09:22:05 -0600

    more es6 destructuring

--- src/index.js
+++ src/index.js
@@ -1,40 +1,59 @@
-import animals, { useAnimals } from "./data";
-
-const [cat, dog] = animals;
-// console.log(cat);
-// const {
-//   name,
-//   sound,
-//   feedingRequirements: { food, water }
-// } = cat;
-// console.log(name);
-
-// console.log(food);
-const [animal, makeSound] = useAnimals(cat);
-console.log(animal);
-makeSound();
-
 /////
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+const [honda, tesla] = cars;
+console.log(honda);
+// const {
+//   speedStats: { topSpeed: hondaTopSpeed }
+// } = honda;
+// const {
+//   speedStats: { topSpeed: teslaTopSpeed }
+// } = tesla;
+// const {
+//   coloursByPopularity: [hondaTopColour]
+// } = honda;
+// const {
+//   coloursByPopularity: [teslaTopColour]
+// } = tesla;
+// const {
+//   speedStats: { zeroToSixty: hondaTopZero }
+// } = honda;
+// const {
+//   speedStats: { zeroToSixty: teslaTopZero }
+// } = tesla;
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+  coloursByPopularity: [hondaTopColour],
+  speedStats: { zeroToSixty: hondaTopZero }
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+  coloursByPopularity: [teslaTopColour],
+  speedStats: { zeroToSixty: teslaTopZero }
+} = tesla;
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+      <th>Zero to Sixty</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+      <td>{teslaTopZero}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+      <td>{hondaTopZero}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
