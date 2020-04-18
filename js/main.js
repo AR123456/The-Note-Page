93672c885a2a1b27cb9837620bfd87b4d1b7d710
AR123456 <coloradoz99@gmail.com>
2020-04-18 08:32:41 -0600

    D3 ordinal scale colors

--- js/main.js
+++ js/main.js
@@ -4,20 +4,48 @@ var svg = d3
   .attr("width", "400")
   .attr("height", "400");
 
-var x = d3
-  // time scales are a type of linear scale
-  // domain works with the JS date object instead of integers
-  // syntax is like that of a linear scale
-  .scaleTime()
-  // create a new date object here by passing in the day time and month we want
-  // to do this with with actual data would need to convert a string of the date to a javascript date object
-  .domain([new Date(2000, 0, 1), new Date(2001, 0, 1)])
-  .range([0, 400]);
+// var color = d3
+//   .scaleOrdinal()
+//   .domain([
+//     "AFRICA",
+//     "N. AMERICA",
+//     "EUROPE",
+//     "S. AMERICA",
+//     "ASIA",
+//     "AUSTRALASIA"
+//   ])
+//   .range(["RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "INDIGO", "GREY"]);
 
-console.log(x(new Date(2000, 7, 1)));
-console.log(x(new Date(2000, 2, 1)));
-console.log(x(new Date(2000, 10, 25)));
+// console.log(color("AFRICA"));
+// console.log(color("N. AMERICA"));
+// console.log(color("EUROPE"));
+// console.log(color("S. AMERICA"));
+// console.log(color("PANGAEA"));
+var color = d3
+  // use this to work with discrete data like a collection of categories
+  // instead of continuous data
+  // use case  to associate categories with different colors but it
+  //could be used to associate any two strings with each other
 
-console.log(x.invert(232.8));
-console.log(x.invert(66.5));
-console.log(x.invert(360));
+  .scaleOrdinal()
+  // domain and rage are always an array of values
+  // the scale will provide a mapping between them based on that position in the array
+
+  .domain([
+    "AFRICA",
+    "N. AMERICA",
+    "EUROPE",
+    "S. AMERICA",
+    "ASIA",
+    "AUSTRALASIA",
+  ])
+  // d3 has own functions to generate colors from the d3 scale chromatic module
+  .range(d3.schemeCategory10);
+
+console.log(color("AFRICA"));
+console.log(color("N. AMERICA"));
+console.log(color("EUROPE"));
+console.log(color("S. AMERICA"));
+// if a value is added to the domain D3 adds it to the range using next avalible
+// if there are more values in domain than range the scale will loop back and start at the begining
+console.log(color("PANGAEA"));
