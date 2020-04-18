788bf9874b6780fd281cd167443b4b33e33317da
AR123456 <coloradoz99@gmail.com>
2020-04-18 08:33:36 -0600

    D3 band scales var chart

--- js/main.js
+++ js/main.js
@@ -1,51 +1,61 @@
+/*
+ Band scales - used to space out rectangles in bar charts- 
+*/
+
 var svg = d3
   .select("#chart-area")
   .append("svg")
   .attr("width", "400")
   .attr("height", "400");
 
-// var color = d3
-//   .scaleOrdinal()
-//   .domain([
-//     "AFRICA",
-//     "N. AMERICA",
-//     "EUROPE",
-//     "S. AMERICA",
-//     "ASIA",
-//     "AUSTRALASIA"
-//   ])
-//   .range(["RED", "ORANGE", "YELLOW", "GREEN", "BLUE", "INDIGO", "GREY"]);
+d3.json("data/buildings.json").then(function (data) {
+  console.log(data);
 
-// console.log(color("AFRICA"));
-// console.log(color("N. AMERICA"));
-// console.log(color("EUROPE"));
-// console.log(color("S. AMERICA"));
-// console.log(color("PANGAEA"));
-var color = d3
-  // use this to work with discrete data like a collection of categories
-  // instead of continuous data
-  // use case  to associate categories with different colors but it
-  //could be used to associate any two strings with each other
+  data.forEach(function (d) {
+    d.height = +d.height;
+  });
+  // changing x scale width - need to make the bands more narrow or the SVG bigger for all to fit
+  // descrete domain, continuous range
+  var x = d3
+    // space out bands of equal length based on how many elements it has in its domain
+    // cannot add new values in the same way as ordinal scale
+    .scaleBand()
+    // categories to display for domain - needed to add to building.json and here in the domain
+    .domain([
+      "Burj Khalifa",
+      "Shanghai Tower",
+      "Abraj Al-Bait Clock Tower",
+      "Ping An Finance Centre",
+      "Lotte World Tower",
+      "One World Trade Center",
+      "Guangzhou CTF Finance Center",
+    ])
+    // the pixle postions that we want chart spaced over
+    // span of SVG
+    .range([0, 400])
+    // padding ratio inbetween the bars in the domain -
+    .paddingInner(0.3)
+    // padding ratio the outside edge of the whole domain
+    .paddingOuter(0.3);
+  console.log(x("Burj Khalifa"));
+  var y = d3.scaleLinear().domain([0, 828]).range([0, 400]);
 
-  .scaleOrdinal()
-  // domain and rage are always an array of values
-  // the scale will provide a mapping between them based on that position in the array
-
-  .domain([
-    "AFRICA",
-    "N. AMERICA",
-    "EUROPE",
-    "S. AMERICA",
-    "ASIA",
-    "AUSTRALASIA",
-  ])
-  // d3 has own functions to generate colors from the d3 scale chromatic module
-  .range(d3.schemeCategory10);
-
-console.log(color("AFRICA"));
-console.log(color("N. AMERICA"));
-console.log(color("EUROPE"));
-console.log(color("S. AMERICA"));
-// if a value is added to the domain D3 adds it to the range using next avalible
-// if there are more values in domain than range the scale will loop back and start at the begining
-console.log(color("PANGAEA"));
+  var rects = svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("y", 0)
+    // return the value from passing the building name into the x scale
+    .attr("x", function (d) {
+      return x(d.name);
+    })
+    // bandwidth  method gives access to width of bands
+    .attr("width", x.bandwidth)
+    .attr("height", function (d) {
+      return y(d.height);
+    })
+    .attr("fill", function (d) {
+      return "grey";
+    });
+});
