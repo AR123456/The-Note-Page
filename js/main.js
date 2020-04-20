5c01d9a1807e415fbddc223a4bdff2e351cb9aab
AR123456 <coloradoz99@gmail.com>
2020-04-20 05:54:06 -0600

    margins and groups

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 /*
- D3 min, max, and extent
- used to automaicaly set the domain for scales - may not always know the min and max in a
- set of data
- map functions to set domains for catigorical data 
- min max and extend all take in 2 arguments - 
- an array of data and a callback function 
- the callback function will loop over every item in the array 
- for each item need to retun the piece of data we are intrested in 
- min max and extent will compare values and return relevant data 
- the min , max  and for extent an array of the min and max values 
-*/
+ Margins and groups
+ SVG Groups - an invisible SVG element for organizing data - using to get vis 
+ to conform to D3 margin convention 
+ margin is defined in main js - can use D3 axis generators
+ attach to svg group element - use to bundle graphical elements together and apply transformations 
 
-const svg = d3
+ Transformation    
+ Translation 
+Margins can give room for the x and y lables
+*/
+// defining margin as an object and passing in our values
+const margin = { left: 100, right: 10, top: 10, bottom: 100 };
+// this will define the size of the group that we are attaching everything to - wont include space for margins
+// 600 x 400 less the margins
+const width = 600 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+// append the group to the canvas and center it
+const g = d3
   .select("#chart-area")
   .append("svg")
-  .attr("width", "400")
-  .attr("height", "400");
+  // removed hard coded values here and added what was defined above
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  // svg groups do not have any visusal presence by themselves ,
+  //they are useful to group graphical elements together and apply transformations to many
+  //shapes at once
+  .append("g")
+  // transform translates the data vis into the center -
+  //shift to left by value of left margin and down by value of top margin
+  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
 d3.json("data/buildings.json").then((data) => {
-  console.log(data);
+  // console.log(data);
 
   data.forEach((d) => {
     d.height = +d.height;
@@ -26,43 +39,32 @@ d3.json("data/buildings.json").then((data) => {
 
   const x = d3
     .scaleBand()
-
     .domain(
-      // map function to generate the array of building names
-      // to add to the domain
-      // useful for band scales and ordinal scales category values
       data.map((d) => {
         return d.name;
       })
     )
-    //range method takes in the min and max of the range
-    .range([0, 400])
+    .range([0, width])
     .paddingInner(0.3)
     .paddingOuter(0.3);
 
   const y = d3
     .scaleLinear()
-    //domain method, takes in the vaule of the min and max of the domain
-    // 0 to the height of the tallest building
     .domain([
       0,
-      // d3 max takes in an array of data as its first argument
-      // the second argument is pointing to the buildings height value
-      // finds the max value in the array to use in the domain function
       d3.max(data, (d) => {
         return d.height;
       }),
     ])
-    //range method takes in the min and max of the range
-    .range([0, 400]);
-  // .extent returns an array of the min and max vaule
-  const rects = svg
-    .selectAll("rect")
-    .data(data)
+    .range([0, height]);
+  // this now gets appended to the group , not the svg
+  const rects = g.selectAll("rect").data(data);
+
+  rects
     .enter()
     .append("rect")
     .attr("y", 0)
-    .attr("x", (d) => {
+    .attr("x", function (d) {
       return x(d.name);
     })
     .attr("width", x.bandwidth)
