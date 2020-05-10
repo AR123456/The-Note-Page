eda07bc6d22e32c491a390b4667b7a03f9f4e49f
AR123456 <coloradoz99@gmail.com>
2020-05-10 11:15:13 -0600

    d3 js gapminder add legend

--- js/main.js
+++ js/main.js
@@ -1,142 +1,150 @@
 /*
- *    main.js
- *  47- Gapminder Clone - clone of bubble chart that shows how
- * population, per capita and life expectancy have changed in different countries over the past 2 centurys
- * // data is an array of years and an array of countries in that year. Each countrie contains its continent,
- * income, life expecnacy and population.
- * 1) update data in cycle based on year
- * 2)Size of cicles changes to reflect population, position on x is GDP Per Capita and postion
- * on Y is life expectancy
- * 3) y axis is a log scale 400 to 40,000
- * 4) ordinal scale to seperate the circles by their continent
- */
+*    main.js
+*    Mastering Data Visualization with D3.js
+*   50- Adding a legend
+*/
 
-// const margin = { left: 80, right: 20, top: 50, bottom: 100 };
-const height = 350,
-  width = 700;
-// console.log(height);
-// console.log(width);
-// console.log(margin);
-const g = d3
-  .select("#chart-area")
-  .append("svg")
-  .attr("width", width + 100)
-  .attr("height", height + 150)
-  .append("g")
-  .attr("transform", "translate(" + 80 + ", " + 50 + ")");
+var margin = { left:80, right:20, top:50, bottom:100 };
+var height = 500 - margin.top - margin.bottom, 
+    width = 800 - margin.left - margin.right;
 
-let time = 0;
+var g = d3.select("#chart-area")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform", "translate(" + margin.left + 
+            ", " + margin.top + ")");
+
+var time = 0;
 
 // Scales
-const x = d3.scaleLog().base(10).range([0, width]).domain([142, 150000]);
-const y = d3.scaleLinear().range([height, 0]).domain([0, 90]);
-const area = d3
-  .scaleLinear()
-  .range([25 * Math.PI, 1500 * Math.PI])
-  .domain([2000, 1400000000]);
-const continentColor = d3.scaleOrdinal(d3.schemePastel1);
+var x = d3.scaleLog()
+    .base(10)
+    .range([0, width])
+    .domain([142, 150000]);
+var y = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, 90]);
+var area = d3.scaleLinear()
+    .range([25*Math.PI, 1500*Math.PI])
+    .domain([2000, 1400000000]);
+var continentColor = d3.scaleOrdinal(d3.schemePastel1);
 
 // Labels
-const xLabel = g
-  .append("text")
-  .attr("y", height + 50)
-  .attr("x", width / 2)
-  .attr("font-size", "20px")
-  .attr("text-anchor", "middle")
-  .text("GDP Per Capita ($)");
-const yLabel = g
-  .append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", -40)
-  .attr("x", -170)
-  .attr("font-size", "20px")
-  .attr("text-anchor", "middle")
-  .text("Life Expectancy (Years)");
-const timeLabel = g
-  .append("text")
-  .attr("y", height - 10)
-  .attr("x", width - 40)
-  .attr("font-size", "40px")
-  .attr("opacity", "0.4")
-  .attr("text-anchor", "middle")
-  .text("1800");
+var xLabel = g.append("text")
+    .attr("y", height + 50)
+    .attr("x", width / 2)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("GDP Per Capita ($)");
+var yLabel = g.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -40)
+    .attr("x", -170)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Life Expectancy (Years)")
+var timeLabel = g.append("text")
+    .attr("y", height -10)
+    .attr("x", width - 40)
+    .attr("font-size", "40px")
+    .attr("opacity", "0.4")
+    .attr("text-anchor", "middle")
+    .text("1800");
 
 // X Axis
-const xAxisCall = d3
-  .axisBottom(x)
-  .tickValues([400, 4000, 40000])
-  .tickFormat(d3.format("$"));
+var xAxisCall = d3.axisBottom(x)
+    .tickValues([400, 4000, 40000])
+    .tickFormat(d3.format("$"));
 g.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")")
-  .call(xAxisCall);
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height +")")
+    .call(xAxisCall);
 
 // Y Axis
-const yAxisCall = d3.axisLeft(y).tickFormat((d) => {
-  return +d;
-});
-g.append("g").attr("class", "y axis").call(yAxisCall);
+var yAxisCall = d3.axisLeft(y)
+    .tickFormat(function(d){ return +d; });
+g.append("g")
+    .attr("class", "y axis")
+    .call(yAxisCall);
 
-d3.json("data/data.json").then((data) => {
-  // console.log(data);
+var continents = ["europe", "asia", "americas", "africa"];
 
-  // Clean data
-  const formattedData = data.map((year) => {
-    return year["countries"]
-      .filter((country) => {
-        const dataExists = country.income && country.life_exp;
-        return dataExists;
-      })
-      .map((country) => {
-        country.income = +country.income;
-        country.life_exp = +country.life_exp;
-        return country;
-      });
-  });
+var legend = g.append("g")
+    .attr("transform", "translate(" + (width - 10) + 
+        "," + (height - 125) + ")");
 
-  // Run the code every 0.1 second
-  d3.interval(() => {
-    // At the end of our data, loop back
-    time = time < 214 ? time + 1 : 0;
-    update(formattedData[time]);
-  }, 1000);
+continents.forEach(function(continent, i){
+    var legendRow = legend.append("g")
+        .attr("transform", "translate(0, " + (i * 20) + ")");
 
-  // First run of the visualization
-  update(formattedData[0]);
+    legendRow.append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", continentColor(continent));
+
+    legendRow.append("text")
+        .attr("x", -10)
+        .attr("y", 10)
+        .attr("text-anchor", "end")
+        .style("text-transform", "capitalize")
+        .text(continent);
 });
 
-function update(data) {
-  // Standard transition time for the visualization
-  const t = d3.transition().duration(100);
+d3.json("data/data.json").then(function(data){
+    console.log(data);
 
-  // JOIN new data with old elements.
-  const circles = g.selectAll("circle").data(data, (d) => {
-    return d.country;
-  });
-
-  // EXIT old elements not present in new data.
-  circles.exit().attr("class", "exit").remove();
-
-  // ENTER new elements present in new data.
-  circles
-    .enter()
-    .append("circle")
-    .attr("class", "enter")
-    .attr("fill", (d) => {
-      return continentColor(d.continent);
-    })
-    .merge(circles)
-    .transition(t)
-    .attr("cy", (d) => {
-      return y(d.life_exp);
-    })
-    .attr("cx", (d) => {
-      return x(d.income);
-    })
-    .attr("r", function (d) {
-      return Math.sqrt(area(d.population) / Math.PI);
+    // Clean data
+    const formattedData = data.map(function(year){
+        return year["countries"].filter(function(country){
+            var dataExists = (country.income && country.life_exp);
+            return dataExists
+        }).map(function(country){
+            country.income = +country.income;
+            country.life_exp = +country.life_exp;
+            return country;            
+        })
     });
 
-  // Update the time label
-  timeLabel.text(+(time + 1800));
+    // Run the code every 0.1 second
+    d3.interval(function(){
+        // At the end of our data, loop back
+        time = (time < 214) ? time+1 : 0
+        update(formattedData[time]);            
+    }, 100);
+
+    // First run of the visualization
+    update(formattedData[0]);
+
+})
+
+function update(data) {
+    // Standard transition time for the visualization
+    var t = d3.transition()
+        .duration(100);
+
+    // JOIN new data with old elements.
+    var circles = g.selectAll("circle").data(data, function(d){
+        return d.country;
+    });
+
+    // EXIT old elements not present in new data.
+    circles.exit()
+        .attr("class", "exit")
+        .remove();
+
+    // ENTER new elements present in new data.
+    circles.enter()
+        .append("circle")
+        .attr("class", "enter")
+        .attr("fill", function(d) { return continentColor(d.continent); })
+        .merge(circles)
+        .transition(t)
+            .attr("cy", function(d){ return y(d.life_exp); })
+            .attr("cx", function(d){ return x(d.income) })
+            .attr("r", function(d){ return Math.sqrt(area(d.population) / Math.PI) });
+
+    // Update the time label
+    timeLabel.text(+(time + 1800))
 }
