1b0a7fdd2cd04ba3f1eb3d3fb0653777008fdbc2
AR123456 <coloradoz99@gmail.com>
2020-12-23 07:34:57 -0700

    book list project

---
+++ book-list-working/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
+
+import BookContextProvider from "./contexts/BookContext";
+
+function App() {
+  return (
+    <div className="App">
+      <BookContextProvider>
+        <Navbar></Navbar>
+        <BookList></BookList>
+        <BookForm></BookForm>
+      </BookContextProvider>
+    </div>
+  );
+}
+
+export default App;
