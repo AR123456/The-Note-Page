f14c65df189cb403b3a3a3e8d4ddbfe581082658
AR123456 <coloradoz99@gmail.com>
2021-05-01 07:39:51 -0600

    set up react hooks react tetris game

--- Tetris-3-Ways-JS-React-Javascript/React-Tetris/src/components/StartButton.js
+++ Tetris-3-Ways-JS-React-Javascript/React-Tetris/src/components/StartButton.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { StyledStartButton } from "./styles/StylesdStartButton";
 
-const StartButton = ({ callback }) => {
-  return <div>Start Button</div>;
-};
-
+const StartButton = ({ callback }) => (
+  <StyledStartButton onClick={callback}>Start Game </StyledStartButton>
+);
 export default StartButton;
--- Tetris-3-Ways-JS-React-Javascript/React-Tetris/src/components/Tetris.js
+++ Tetris-3-Ways-JS-React-Javascript/React-Tetris/src/components/Tetris.js
@@ -1,20 +1,34 @@
-import React from "react";
-import { createStage } from "../gameHelpers";
+import React, { useState } from "react";
+
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
+
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(null);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log("re-rendered");
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()}></Stage>
+        <Stage stage={stage}></Stage>
         <aside>
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over"></Display>
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
           <StartButton></StartButton>
         </aside>
       </StyledTetris>
