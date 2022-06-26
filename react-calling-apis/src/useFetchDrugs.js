489c2f14549953286369aebfe42bda49f9545888
AR123456 <coloradoz99@gmail.com>
2022-06-26 15:28:29 -0600

    call in use effect

--- react-calling-apis/src/useFetchDrugs.js
+++ react-calling-apis/src/useFetchDrugs.js
@@ -1,13 +1,99 @@
+// useReducer to handle state , useEffect to re load when something on page changes
 import { useReducer, useEffect } from "react";
 import axios from "axios";
 
+///////////// this is an object that defines our actions so we can ref them later
+//( this can is often is put into a seperate file )
+const ACTIONS = {
+  MAKE_REQUEST: "make-request",
+  GET_DATA: "get-data",
+  ERROR: "error",
+};
+/// this is a url to use in developemnt to get round cors error
+const URL_FOR_DEV = "https://api.allorigins.win/raw?url=";
+//   https://api.allorigins.win/raw?url=
+
+let search = "Lasix";
+// const BASE_URL =
+//   URL_FOR_DEV +
+//   "https://api.fda.gov/drug/label.json?search=openfda.brand_name:" +
+//   search +
+//   "&limit=1";
+const BASE_URL =
+  "https://api.allorigins.win/raw?url=https://api.fda.gov/drug/label.json?search=openfda.brand_name:" +
+  search +
+  "&limit=1";
+// //////////////this reducer function takes in state and action
+// reducer will be called every time we call dispatch
+// and dispatch, whatever we pass to it is populated inside
+// the action variable.  State is whatever the current state
+// of the application is
+function reducer(state, action) {
+  // swithch statement that makes user of actions
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      // when making a new request set loading true and clear out whatever drugs were in state
+      return { loading: true, drugs: [] };
+    case ACTIONS.GET_DATA:
+      // spread everything that is in current state into new state, loading false
+      // and pass jobs in on the payload of our action and set that inside of our
+      // action.getdata
+      //jobs too action.payload.jobs
+      return { ...state, loading: false, jobs: action.payload.jobs };
+    case ACTIONS.ERROR:
+      // pass the error object in, clear out jobs if there is an error
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+    // if none of the above just return our current state
+    default:
+      return state;
+  }
+}
 const useFetchDrugs = (params) => {
+  // useReducer takes in the reducer function and initial state - in this case
+  // an object that is empty array of jobs
+  const [state, dispatch] = useReducer(reducer, { drugs: [], loading: true });
+  // useEffect hook to controle when state updates
+  useEffect(() => {
+    // in dispatch, by convention
+    // types are actions, payloads is the data for the type
+    // frist   state - set state to loading state
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    // second call axios  - get data
+    axios
+      .get(BASE_URL, {
+        //pass in some params
+        // markdown true will make descriptions appear in markdow so  easier to work with
+        // this may not be needed for the FDA API
+        params: { markdown: true, ...params },
+      })
+      // using .then syntax here vs async await which could have been used
+      .then((res) => {
+        //res.data  is our response back from the API
+        //put it into our get data dispatch
+        // we got the data,lets save it to state by calling the get data action
+        dispatch({ type: ACTIONS.GET_DATA, payload: { drugs: res.data } });
+      })
+      // catch the error
+      .catch((e) => {
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+
+    // this use effect will run any time a params changes
+  }, [params]);
+
   // just returning a static object
-  return {
-    drugs: [],
-    loading: false,
-    error: false,
-  };
+  //   return {
+  //     drugs: [],
+  //     loading: false,
+  //     error: false,
+  //   };
+  /////// here now returning state
+  return state;
   //   (
   //     <div>
   //       <h2>Here are the drugs</h2>
