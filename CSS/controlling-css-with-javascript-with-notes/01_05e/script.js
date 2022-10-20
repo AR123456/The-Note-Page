9a4667ff1e74980aac6abcfa9e5659b5876007a7
AR123456 <coloradoz99@gmail.com>
2022-10-20 06:25:42 -0600

    on click change style sheet

--- CSS/controlling-css-with-javascript-with-notes/01_05e/script.js
+++ CSS/controlling-css-with-javascript-with-notes/01_05e/script.js
@@ -12,6 +12,7 @@ const targetSheet = styleSheetsArray.find((styleSheet) => {
 // injecting new rules into style sheet using javascript
 console.log(targetSheet);
 //https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet/insertRule
+// inject at index 1
 targetSheet.insertRule(".animals li a {border-color: green}", 1);
 // remove rule from stylesheet using javascript
 //https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleSheet/deleteRule
--- CSS/controlling-css-with-javascript-with-notes/01_06/script.js
+++ CSS/controlling-css-with-javascript-with-notes/01_06/script.js
@@ -1,5 +1,32 @@
 const styleSheets = document.styleSheets;
 const listButton = document.getElementById("list");
 const gridButton = document.getElementById("grid");
-
+// RODO
+// Disable grid style sheet on load
+// add button funtionality to toggle grid when selected
+// highlight the current layout button
 listButton.classList.add("current");
+// target and disable stylesheet
+const styleSheetsArray = Array.from(styleSheets);
+const gridStyleSheet = styleSheetsArray.find((styleSheet) => {
+  if (styleSheet?.href) {
+    const pathName = new URL(styleSheet.href).pathname.split("/").pop();
+    if (pathName === "grid.css") {
+      return styleSheet;
+    }
+  }
+});
+gridStyleSheet.disabled = true;
+
+// on clicking grid view set disabled to false
+gridButton.addEventListener("click", (e) => {
+  gridStyleSheet.disabled = false;
+  gridButton.classList.add("current");
+  listButton.classList.remove("current");
+});
+
+listButton.addEventListener("click", (e) => {
+  gridStyleSheet.disabled = true;
+  gridButton.classList.remove("current");
+  listButton.classList.add("current");
+});
