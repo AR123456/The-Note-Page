998b72962b4f8726d48ee05a7ea36ce769b8603a
AR123456 <coloradoz99@gmail.com>
2022-10-20 05:30:54 -0600

    css set disabled to true

--- CSS/controlling-css-with-javascript-with-notes/01_03e/script.js
+++ CSS/controlling-css-with-javascript-with-notes/01_03e/script.js
@@ -11,4 +11,10 @@ const targetSheet = styleSheetsArray.find((styleSheet) => {
   }
 });
 console.log(targetSheet);
+// now that we have the object can set it to disabled
+//https://developer.mozilla.org/en-US/docs/Web/API/StyleSheet/disabled
+// could be used if there are elements on page that do not need to be used uless they are active
+// or if shipping a page with diffrent color schemes / layouts and the user can choose which to apply
+// like dark mode
+
 targetSheet.disabled = true;
