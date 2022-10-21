44d61ab927ffdad48bb020ec150ab75a420571a0
AR123456 <coloradoz99@gmail.com>
2022-10-21 07:11:58 -0600

    use js to get computed value

--- CSS/controlling-css-with-javascript-with-notes/02_02e/script.js
+++ CSS/controlling-css-with-javascript-with-notes/02_02e/script.js
@@ -1,7 +1,14 @@
 const heading = document.querySelector(".masthead h1");
+// computed styles are what has been applied by all means
+// can get this in dev tools  or here with JS using getComputedStu;e
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+
 const computedStyles = window.getComputedStyle(heading);
-
+// dig into the massive object returned
+// the JS Name
 console.log(computedStyles.fontSize);
-
+//with getPropertyValue - an array like object
+//https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/getPropertyValue
+// the CSS name
+// pass in the CSS property in parens - use actual name here
 console.log(computedStyles.getPropertyValue("font-size"));
-
