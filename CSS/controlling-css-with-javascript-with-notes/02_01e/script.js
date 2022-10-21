11bdf361767f3975bea2786fbad28baf5776374b
AR123456 <coloradoz99@gmail.com>
2022-10-21 06:56:03 -0600

    use js to modify style sheet rule

--- CSS/controlling-css-with-javascript-with-notes/02_01e/script.js
+++ CSS/controlling-css-with-javascript-with-notes/02_01e/script.js
@@ -11,5 +11,10 @@ const targetSheet = styleSheetsArray.find((styleSheet) => {
 });
 
 console.log(targetSheet);
+//go to specific style decloration or rule
+// in the extra.css file 0 is the first rule here changing that with JS by adding a background color and border radis
+// using .style is like it is used for in line styling, here using it to inject style with js
+// have to use camel case with no - in JS  for CSS properties that contain a -
 targetSheet.cssRules[0].style.backgroundColor = "hsl(120, 57%, 20%)";
+// can add new or over ride
 targetSheet.cssRules[0].style.borderRadius = "1rem";
