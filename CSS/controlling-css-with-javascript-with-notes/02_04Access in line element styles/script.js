be91a9efac13e546c1c6a68b6614eaa0a96b1604
AR123456 <coloradoz99@gmail.com>
2022-10-22 04:47:05 -0600

    log in line style elements

--- CSS/controlling-css-with-javascript-with-notes/02_04Access in line element styles/script.js
+++ CSS/controlling-css-with-javascript-with-notes/02_04Access in line element styles/script.js
@@ -1,6 +1,8 @@
+// target the 5th thing in the list
 const listItem = document.querySelector(".animals li:nth-child(5) a");
-
+// change its style
 listItem.style.backgroundColor = "orange";
-
+// log out the styles on the element
+// array like list of avalable styles
 console.log(listItem.style);
 console.log(listItem.style.backgroundColor);
