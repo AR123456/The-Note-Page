05b2d57e2d218c14d16a919fe09812faba44601d
AR123456 <coloradoz99@gmail.com>
2022-10-23 05:53:54 -0600

    set existing property to create new

--- CSS/controlling-css-with-javascript-with-notes/03_03Set existing property val to creat new/script.js
+++ CSS/controlling-css-with-javascript-with-notes/03_03Set existing property val to creat new/script.js
@@ -1,6 +1,11 @@
 const styleSheet = document.styleSheets[0];
 
 styleSheet.cssRules[2].style.setProperty("background-color", "skyblue");
+//
 console.log(styleSheet.cssRules[2].style.getPropertyValue("background-color"));
+//using css setProperty
 styleSheet.cssRules[2].style.setProperty("max-inline-size", "65rem");
+// could also use this way
+// styleSheet.cssRules[2].style.maxInlineSize="65rem";
+
 console.log(styleSheet.cssRules[2].style.getPropertyValue("max-inline-size"));
