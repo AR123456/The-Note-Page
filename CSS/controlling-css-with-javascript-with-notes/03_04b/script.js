93fa91e9b6c64a33a272ee02354d1e8971822557
AR123456 <coloradoz99@gmail.com>
2022-10-23 06:00:06 -0600

    remove a css property with js

--- CSS/controlling-css-with-javascript-with-notes/03_04b/script.js
+++
@@ -1,6 +0,0 @@
-const styleSheet = document.styleSheets[0];
-
-styleSheet.cssRules[2].style.setProperty("background-color", "skyblue");
-console.log(styleSheet.cssRules[2].style.getPropertyValue("background-color"));
-styleSheet.cssRules[2].style.setProperty("max-inline-size", "65rem");
-console.log(styleSheet.cssRules[2].style.getPropertyValue("max-inline-size"));
--- CSS/controlling-css-with-javascript-with-notes/03_04remove spec property/script.js
+++ CSS/controlling-css-with-javascript-with-notes/03_04remove spec property/script.js
@@ -1,5 +1,6 @@
 const styleSheet = document.styleSheets[0];
-
+// can user CRUD like operations on the style sheet interface
+// properties and rules --CRUD
 // Ruleset text before:
 console.log(styleSheet.cssRules[2].cssText);
 
@@ -7,7 +8,8 @@ styleSheet.cssRules[2].style.setProperty("background-color", "skyblue");
 console.log(styleSheet.cssRules[2].style.getPropertyValue("background-color"));
 styleSheet.cssRules[2].style.setProperty("max-inline-size", "65rem");
 console.log(styleSheet.cssRules[2].style.getPropertyValue("max-inline-size"));
+// D of crud remove this property
 styleSheet.cssRules[2].style.removeProperty("margin-inline");
 
-// Ruleset text after:
+// Ruleset text after: -current live version as del by browser
 console.log(styleSheet.cssRules[2].cssText);
