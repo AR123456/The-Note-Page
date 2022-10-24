a6f03872ee219cdc4c51b4834113345bcf91f9c1
AR123456 <coloradoz99@gmail.com>
2022-10-24 07:22:22 -0600

    challenge 3

--- CSS/controlling-css-with-javascript-with-notes/03_09 Chal solution/script.js
+++
@@ -1,6 +0,0 @@
-const box = document.querySelector(".box");
-const control = document.querySelector("#degrees");
-
-control.addEventListener("change", () => {
-  box.style.transform = `rotate(${control.value}deg)`;
-});
