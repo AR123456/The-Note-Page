b82c80a51142dea67185ef2827242fbb2ea4d0ef
AR123456 <coloradoz99@gmail.com>
2022-10-24 07:39:37 -0600

    solution

--- CSS/controlling-css-with-javascript-with-notes/03_09Challenge solution/script.js
+++ CSS/controlling-css-with-javascript-with-notes/03_09Challenge solution/script.js
@@ -1,6 +1,6 @@
 const box = document.querySelector(".box");
 const control = document.querySelector("#degrees");
-
+// when the value changes   on change event set the box transition to the value in the control box
 control.addEventListener("change", () => {
   box.style.transform = `rotate(${control.value}deg)`;
 });
