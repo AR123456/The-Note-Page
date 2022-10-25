ead080e13f2ddf11280d084e8a1d73fb4d8c7a2a
AR123456 <coloradoz99@gmail.com>
2022-10-25 06:45:50 -0600

    Css cust prop

--- CSS/controlling-css-with-javascript-with-notes/04_02e/script.js
+++
@@ -1,9 +0,0 @@
-const listItem = document.querySelector(".animals li:nth-child(6) a");
-console.log(listItem.style.cssText);
-listItem.style.setProperty("--color", "skyblue");
-console.log(listItem.style.cssText);
-
-const styleSheet = document.styleSheets[0];
-console.log(styleSheet.cssRules[0].style.cssText);
-styleSheet.cssRules[0].style.setProperty("--border-color", "hsl(53, 100%, 50%)");
-console.log(styleSheet.cssRules[0].style.cssText);
