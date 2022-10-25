241ae5d93a45db3dde1ba057b45e292e8092a04d
AR123456 <coloradoz99@gmail.com>
2022-10-25 06:47:05 -0600

    Register Custom Properties

---
+++ CSS/controlling-css-with-javascript-with-notes/04_03Registered custom properties/script.js
@@ -0,0 +1,9 @@
+// in JS can use
+CSS.registerProperty({
+  name: "--somesize",
+  syntax: "<length>",
+  inherits: true,
+  initialValue: "10mm",
+});
+// in CSS use @property rule
+// https://developer.mozilla.org/en-US/docs/Web/API/CSS/RegisterProperty#:~:text=Registering%20a%20custom%20property%20allows,of%20the%20custom%20property%20is.
