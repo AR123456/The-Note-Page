1a1660caa439df74e55efd1c82ef388a366b3158
AR123456 <coloradoz99@gmail.com>
2022-10-26 06:29:25 -0600

    defining registered custom properties

---
+++ CSS/controlling-css-with-javascript-with-notes/04_04Defining registered custom properties/script.js
@@ -0,0 +1,23 @@
+window.CSS.registerProperty({
+  name: "--font-size",
+  syntax: "<length>",
+  inherits: false,
+  initialValue: "16px",
+});
+// https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Houdini
+window.CSS.registerProperty({
+  name: "--hue",
+  //syntax is the type of value the custom property will capture in its value
+  // list of supported names
+  syntax: "<integer>",
+  inherits: false,
+  initialValue: "20",
+});
+
+const styleSheet = document.styleSheets[0];
+console.log(styleSheet);
+
+console.log(styleSheet.cssRules[4].style.getPropertyValue("--font-size"));
+
+console.log(styleSheet.cssRules[4].styleMap.get("--font-size").value);
+console.log(styleSheet.cssRules[4].styleMap.get("--font-size").unit);
