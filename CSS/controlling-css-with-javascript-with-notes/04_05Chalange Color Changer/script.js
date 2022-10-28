7c3fbf2cd9a93a973e1a5e48c1b7874ab37f4ebc
AR123456 <coloradoz99@gmail.com>
2022-10-28 05:47:12 -0600

    solution to text readablity

--- CSS/controlling-css-with-javascript-with-notes/04_05Chalange Color Changer/script.js
+++ CSS/controlling-css-with-javascript-with-notes/04_05Chalange Color Changer/script.js
@@ -6,10 +6,7 @@ const colorSwatch = document.querySelector(".color-swatch");
 
 const rootRules = document.styleSheets[0].cssRules[0];
 // task
-// get value of each slider and apply to background
-// display the HSLA in the box
-// change the saturation and lightness alpha appropriately
-/////
+
 // capture inputs from four selectors
 // set custom property value based on input
 // change color swatch background color to match input
@@ -26,58 +23,33 @@ const updateDisplay = () => {
     "--lightness"
   )}%, ${currentColors.getPropertyValue("--alpha")});`;
 };
+const updateTextDisplay = () => {
+  console.log(color);
+  colorSwatch.style.color = `hsla(${currentColors.getPropertyValue(
+    "--hue"
+  )}, ${currentColors.getPropertyValue(
+    "--saturation"
+  )}%, ${currentColors.getPropertyValue(
+    "--lightness"
+  )}%, ${currentColors.getPropertyValue("--alpha")});`;
+};
 
 hue.addEventListener("input", () => {
-  // console.log(hue.value);
-  // property name and the value
   rootRules.style.setProperty("--hue", hue.value);
-  console.log(currentColors.getPropertyValue("--hue"));
   updateDisplay();
-  // colorSwatch.innerHTML = `hsla(${currentColors.getPropertyValue(
-  //   "--hue"
-  // )}, ${currentColors.getPropertyValue(
-  //   "--saturation"
-  // )}%, ${currentColors.getPropertyValue(
-  //   "--lightness"
-  // )}%, ${currentColors.getPropertyValue("--alpha")});`;
-  console.log(colorSwatch.innerHTML);
 });
 saturation.addEventListener("input", () => {
-  console.log(saturation.value);
   rootRules.style.setProperty("--saturation", saturation.value);
   updateDisplay();
-  // colorSwatch.innerHTML = `hsla(${currentColors.getPropertyValue(
-  //   "--hue"
-  // )}, ${currentColors.getPropertyValue(
-  //   "--saturation"
-  // )}%, ${currentColors.getPropertyValue(
-  //   "--lightness"
-  // )}%, ${currentColors.getPropertyValue("--alpha")});`;
 });
 lightness.addEventListener("input", () => {
-  console.log(lightness.value);
   rootRules.style.setProperty("--lightness", lightness.value);
   updateDisplay();
-  // colorSwatch.innerHTML = `hsla(${currentColors.getPropertyValue(
-  //   "--hue"
-  // )}, ${currentColors.getPropertyValue(
-  //   "--saturation"
-  // )}%, ${currentColors.getPropertyValue(
-  //   "--lightness"
-  // )}%, ${currentColors.getPropertyValue("--alpha")});`;
 });
 alpha.addEventListener("input", () => {
-  // console.log(alpha.value);
-  console.log(parseInt(alpha.value) / 100);
   alphaNew = parseInt(alpha.value) / 100;
   rootRules.style.setProperty("--alpha", alphaNew);
   updateDisplay();
-  // colorSwatch.innerHTML = `hsla(${currentColors.getPropertyValue(
-  //   "--hue"
-  // )}, ${currentColors.getPropertyValue(
-  //   "--saturation"
-  // )}%, ${currentColors.getPropertyValue(
-  //   "--lightness"
-  // )}%, ${currentColors.getPropertyValue("--alpha")});`;
+  updateTextDisplay();
 });
 // console.log(window.getComputedStyle(colorSwatch));
