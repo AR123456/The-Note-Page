fbeb2a6f7cc8e21d9bc3039344d0963af0a9dc55
AR123456 <coloradoz99@gmail.com>
2022-11-13 05:31:32 -0700

    form inputs

---
+++ HTML Forms/Form-Inputs/script.js
@@ -0,0 +1,11 @@
+//web dev form validation
+// https://www.youtube.com/watch?v=In0nB0ABaUk
+// webdev all about forms
+// https://www.youtube.com/watch?v=fNcJuPIZ2WE
+
+// puting on the same page vs on results.html
+// const resultsList = document.getElementById("results");
+// new URLSearchParams(window.location.search).forEach((value, name) => {
+//   resultsList.append(`${name}: ${value}`);
+//   resultsList.append(document.createElement("br"));
+// });
