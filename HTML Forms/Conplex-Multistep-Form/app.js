a33ef3087b86d96ec86ae69a2cc6b547ac98e960
AR123456 <coloradoz99@gmail.com>
2022-11-15 06:35:46 -0700

    finished complex multistep form

--- HTML Forms/Conplex-Multistep-Form/app.js
+++ HTML Forms/Conplex-Multistep-Form/app.js
@@ -5,31 +5,43 @@
 // getting by data attribute  - The syntax is simple. Any attribute on any element whose attribute name starts with data- is a data attribute.
 //https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
 const multiStepForm = document.querySelector("[data-multi-step]");
-// this needs to be an array
 const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
-
 let currentStep = formSteps.findIndex((step) => {
   return step.classList.contains("active");
-  // get the data step number and use parseInt go make it a number
 });
 
-// if current step is not a number
 if (currentStep < 0) {
   currentStep = 0;
   showCurrentStep();
 }
 
 multiStepForm.addEventListener("click", (e) => {
+  let incrementor;
   if (e.target.matches("[data-next]")) {
-    currentStep += 1;
+    incrementor = 1;
   } else if (e.target.matches("[data-previous]")) {
-    currentStep -= 1;
+    incrementor = -1;
   }
-  showCurrentStep;
+
+  if (incrementor == null) return;
+
+  const inputs = [...formSteps[currentStep].querySelectorAll("input")];
+  const allValid = inputs.every((input) => input.reportValidity());
+  if (allValid) {
+    currentStep += incrementor;
+    showCurrentStep();
+  }
+});
+
+formSteps.forEach((step) => {
+  step.addEventListener("animationend", (e) => {
+    formSteps[currentStep].classList.remove("hide");
+    e.target.classList.toggle("hide", !e.target.classList.contains("active"));
+  });
 });
 
 function showCurrentStep() {
   formSteps.forEach((step, index) => {
-    step.classList.toggle("active", index == currentStep);
+    step.classList.toggle("active", index === currentStep);
   });
 }
