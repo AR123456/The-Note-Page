2410d93f04d2ed5e20855a6c07823f291742392e
AR123456 <coloradoz99@gmail.com>
2022-11-15 06:31:34 -0700

    multi step form

--- HTML Forms/Conplex-Multistep-Form/app.js
+++ HTML Forms/Conplex-Multistep-Form/app.js
@@ -7,10 +7,29 @@
 const multiStepForm = document.querySelector("[data-multi-step]");
 // this needs to be an array
 const formSteps = [...multiStepForm.querySelectorAll("[data-step]")];
-let currentStep = parseInt(
-  formSteps.find((step) => {
-    return step.classList.contains("active");
-    // get the data step number and use parseInt go make it a number
-  })?.dataset.step
-);
-console.log(currentStep);
+
+let currentStep = formSteps.findIndex((step) => {
+  return step.classList.contains("active");
+  // get the data step number and use parseInt go make it a number
+});
+
+// if current step is not a number
+if (currentStep < 0) {
+  currentStep = 0;
+  showCurrentStep();
+}
+
+multiStepForm.addEventListener("click", (e) => {
+  if (e.target.matches("[data-next]")) {
+    currentStep += 1;
+  } else if (e.target.matches("[data-previous]")) {
+    currentStep -= 1;
+  }
+  showCurrentStep;
+});
+
+function showCurrentStep() {
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index == currentStep);
+  });
+}
