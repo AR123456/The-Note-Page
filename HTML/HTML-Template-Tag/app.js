399fcf4bdacffc89437b9ea5bbb1febcae653f16
AR123456 <coloradoz99@gmail.com>
2022-11-18 06:15:39 -0700

    HTML template element

--- HTML/HTML-Template-Tag/app.js
+++ HTML/HTML-Template-Tag/app.js
@@ -1,1 +1,13 @@
 //  https://blog.webdevsimplified.com/2020-06/template-tag/
+const template = document.getElementById("list-item-template");
+const list = document.getElementById("list");
+const button = document.getElementById("add-item");
+let itemCount = list.children.length;
+
+button.addEventListener("click", () => {
+  const item = template.content.cloneNode(true);
+  itemCount++;
+  item.querySelector(".title").innerText = `Item ${itemCount}`;
+  item.querySelector(".content").innerText = `Content ${itemCount}`;
+  list.append(item);
+});
