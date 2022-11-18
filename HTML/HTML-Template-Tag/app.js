933e20fd30ab605e73afdd6dfebf74b4f43893ac
AR123456 <coloradoz99@gmail.com>
2022-11-18 06:30:19 -0700

    HTML Template tag

--- HTML/HTML-Template-Tag/app.js
+++ HTML/HTML-Template-Tag/app.js
@@ -2,12 +2,19 @@
 const template = document.getElementById("list-item-template");
 const list = document.getElementById("list");
 const button = document.getElementById("add-item");
-let itemCount = list.children.length;
+// list.children includes th button and template so remove the.
+let itemCount = list.children.length - 2;
+
+// console.log(list.children);
+// console.log(itemCount);
 
 button.addEventListener("click", () => {
+  // take content in li and clone it
   const item = template.content.cloneNode(true);
   itemCount++;
+  // change title and content
   item.querySelector(".title").innerText = `Item ${itemCount}`;
   item.querySelector(".content").innerText = `Content ${itemCount}`;
+  // add to page
   list.append(item);
 });
