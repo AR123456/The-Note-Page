23e69e6a4f49c724b92fd854c197831a1420b801
AR123456 <coloradoz99@gmail.com>
2022-11-19 06:49:09 -0700

    More complex HTML slider

--- HTML/HTML-Range-Slider/app.js
+++ HTML/HTML-Range-Slider/app.js
@@ -1,11 +1,20 @@
 const range = document.getElementById("range");
 range.addEventListener("input", (e) => {
-  // use + to make the value a number
+  // + makes this a number
   const value = +e.target.value;
-  // label is the next element after the range
   const label = e.target.nextElementSibling;
+  //   getComputedStyle gets the width from the CSS
+  //   target here is the input
+  const range_width = getComputedStyle(e.target).getPropertyValue("width");
+  const label_width = getComputedStyle(label).getPropertyValue("width");
+  ////  this code moves the box along with the slider circle /////
+  //   remove the px
+  const num_width = +range_width.substring(0, range_width.length - 2);
+  const num_label_width = +label_width.substring(0, label_width.length - 2);
+  //   get min and max from HTML and make a number
+  const max = +e.target.max;
+  const min = +e.target.min;
+
+  // put the value in the box
   label.innerHTML = value;
-  // make the position of the element follow the range
-  // calculate the position of the label and have it follow the thumb
-  // doing this with JS
 });
