962a4cf408e2bedef22def64b167f5a556092cd9
AR123456 <coloradoz99@gmail.com>
2022-11-19 06:21:26 -0700

    HTML Range slider

---
+++ HTML/HTML-Range-Slider/app.js
@@ -0,0 +1,10 @@
+const range = document.getElementById("range");
+range.addEventListener("input", (e) => {
+  // use + to make the value a number
+  const value = +e.target.value;
+  // label is the next element after the range
+  const label = e.target.nextElementSibling;
+  label.innerHTML = value;
+  // make the position of the element follow the range
+  // calculate the position of the label and have it follow the thumb
+});
