c0a280583f86fbf5f935739ef6585c7cf4b61ee1
AR123456 <coloradoz99@gmail.com>
2022-12-06 06:53:55 -0700

    starting over

--- CSS/Range-Slider-With-JS-Marnauzq/app.js
+++ CSS/Range-Slider-With-JS-Marnauzq/app.js
@@ -4,79 +4,88 @@ function init() {
   const sliders = document.getElementsByClassName("tick-slider-input");
 
   for (let slider of sliders) {
-    // slider.oninput = onSliderInput;
+    slider.oninput = onSliderInput;
 
-    // updateValue(slider);
-    // updateValuePosition(slider);
-    // updateLabels(slider);
-    // updateProgress(slider);
-    // draw the tick marks on the page
+    updateValue(slider);
+    updateValuePosition(slider);
+    updateLabels(slider);
+    updateProgress(slider);
+
     setTicks(slider);
   }
 }
 
 function onSliderInput(event) {
-  // updateValue(event.target);
-  // updateValuePosition(event.target);
-  // updateLabels(event.target);
-  // updateProgress(event.target);
+  updateValue(event.target);
+  updateValuePosition(event.target);
+  updateLabels(event.target);
+  updateProgress(event.target);
 }
 
 function updateValue(slider) {
-  // let value = document.getElementById(slider.dataset.valueId);
-  // value.innerHTML = "<div>" + slider.value + "</div>";
+  let value = document.getElementById(slider.dataset.valueId);
+
+  value.innerHTML = "<div>" + slider.value + "</div>";
 }
 
 function updateValuePosition(slider) {
-  // let value = document.getElementById(slider.dataset.valueId);
-  // const percent = getSliderPercent(slider);
-  // const sliderWidth = slider.getBoundingClientRect().width;
-  // const valueWidth = value.getBoundingClientRect().width;
-  // const handleSize = slider.dataset.handleSize;
-  // let left =
-  //   percent * (sliderWidth - handleSize) + handleSize / 2 - valueWidth / 2;
-  // left = Math.min(left, sliderWidth - valueWidth);
-  // left = slider.value === slider.min ? 0 : left;
-  // value.style.left = left + "px";
+  let value = document.getElementById(slider.dataset.valueId);
+
+  const percent = getSliderPercent(slider);
+
+  const sliderWidth = slider.getBoundingClientRect().width;
+  const valueWidth = value.getBoundingClientRect().width;
+  const handleSize = slider.dataset.handleSize;
+
+  let left =
+    percent * (sliderWidth - handleSize) + handleSize / 2 - valueWidth / 2;
+
+  left = Math.min(left, sliderWidth - valueWidth);
+  left = slider.value === slider.min ? 0 : left;
+
+  value.style.left = left + "px";
 }
 
 function updateLabels(slider) {
-  // const value = document.getElementById(slider.dataset.valueId);
-  // const minLabel = document.getElementById(slider.dataset.minLabelId);
-  // const maxLabel = document.getElementById(slider.dataset.maxLabelId);
-  // const valueRect = value.getBoundingClientRect();
-  // const minLabelRect = minLabel.getBoundingClientRect();
-  // const maxLabelRect = maxLabel.getBoundingClientRect();
-  // const minLabelDelta = valueRect.left - minLabelRect.left;
-  // const maxLabelDelta = maxLabelRect.left - valueRect.left;
-  // const deltaThreshold = 32;
-  // if (minLabelDelta < deltaThreshold) minLabel.classList.add("hidden");
-  // else minLabel.classList.remove("hidden");
-  // if (maxLabelDelta < deltaThreshold) maxLabel.classList.add("hidden");
-  // else maxLabel.classList.remove("hidden");
+  const value = document.getElementById(slider.dataset.valueId);
+  const minLabel = document.getElementById(slider.dataset.minLabelId);
+  const maxLabel = document.getElementById(slider.dataset.maxLabelId);
+
+  const valueRect = value.getBoundingClientRect();
+  const minLabelRect = minLabel.getBoundingClientRect();
+  const maxLabelRect = maxLabel.getBoundingClientRect();
+
+  const minLabelDelta = valueRect.left - minLabelRect.left;
+  const maxLabelDelta = maxLabelRect.left - valueRect.left;
+
+  const deltaThreshold = 32;
+
+  if (minLabelDelta < deltaThreshold) minLabel.classList.add("hidden");
+  else minLabel.classList.remove("hidden");
+
+  if (maxLabelDelta < deltaThreshold) maxLabel.classList.add("hidden");
+  else maxLabel.classList.remove("hidden");
 }
 
 function updateProgress(slider) {
-  // let progress = document.getElementById(slider.dataset.progressId);
-  // const percent = getSliderPercent(slider);
-  // progress.style.width = percent * 100 + "%";
+  let progress = document.getElementById(slider.dataset.progressId);
+  const percent = getSliderPercent(slider);
+
+  progress.style.width = percent * 100 + "%";
 }
 
 function getSliderPercent(slider) {
-  // const range = slider.max - slider.min;
-  // const absValue = slider.value - slider.min;
-  // return absValue / range;
+  const range = slider.max - slider.min;
+  const absValue = slider.value - slider.min;
+
+  return absValue / range;
 }
-// This function is drawing the tick marks onto the page
+
 function setTicks(slider) {
   let container = document.getElementById(slider.dataset.tickId);
-  console.log(container);
   const spacing = parseFloat(slider.dataset.tickStep);
-  // console.log("spacing" + spacing);
   const sliderRange = slider.max - slider.min;
-  // console.log(sliderRange);
   const tickCount = sliderRange / spacing + 1; // +1 to account for 0
-  // console.log(tickCount);
 
   for (let ii = 0; ii < tickCount; ii++) {
     let tick = document.createElement("span");
@@ -88,10 +97,11 @@ function setTicks(slider) {
 }
 
 function onResize() {
-  // const sliders = document.getElementsByClassName("tick-slider-input");
-  // for (let slider of sliders) {
-  //   updateValuePosition(slider);
-  // }
+  const sliders = document.getElementsByClassName("tick-slider-input");
+
+  for (let slider of sliders) {
+    updateValuePosition(slider);
+  }
 }
 
 window.onload = init;
