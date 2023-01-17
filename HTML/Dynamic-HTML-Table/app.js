cc6a3f44c069274248513076b1c0a4a0dfe50863
AR123456 <coloradoz99@gmail.com>
2023-01-17 06:32:58 -0700

    dynamic table

--- HTML/Dynamic-HTML-Table/app.js
+++ HTML/Dynamic-HTML-Table/app.js
@@ -1,12 +1,29 @@
 function generateTable() {
   // creates a <table> element and a <tbody> element
   const tbl = document.createElement("table");
+  const tblHead = document.createElement("thead");
   const tblBody = document.createElement("tbody");
+  const row = document.createElement("tr");
+  const rowTH = document.createElement("th");
+  // create th head with header rows
+  rowTH.innerHTML = `
+  <th>${"Company"}</th>
+  <th>${"Contact"}</th>
+  <th>${"Country"}</th>
+  <th><a href="#" class="btn btn-danger btn-sm delete">X</a></th>
+`;
+  tblHead.appendChild(rowTH);
 
+  row.innerHTML = `
+  <td>${"book.title"}</td>
+  <td>${"book.author"}</td>
+  <td>${"book.isbn"}</td>
+  <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+`;
+  tblBody.appendChild(row);
   // creating all cells
   for (let i = 0; i < 2; i++) {
     // creates a table row
-    const row = document.createElement("tr");
 
     for (let j = 0; j < 2; j++) {
       // Create a <td> element and a text node, make the text
@@ -33,17 +50,3 @@ function generateTable() {
 // https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
 
 // snipit from Brads https://www.youtube.com/watch?v=JaMCxVWtW58&t=2s
-function addBookToList(book) {
-  const list = document.querySelector('#book-list');
-
-  const row = document.createElement('tr');
-
-  row.innerHTML = `
-    <td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-  `;
-
-  list.appendChild(row);
-}
