313aeffbea880a54a064901a30d080b73e2668c2
AR123456 <coloradoz99@gmail.com>
2023-01-18 06:46:53 -0700

    dynamic table header

--- HTML/Dynamic-HTML-Table/app.js
+++ HTML/Dynamic-HTML-Table/app.js
@@ -41,8 +41,50 @@ const growthByYearNeededToBeSaved = [
   "9245.41",
   "10175.38",
 ];
+ppmt = 75.0;
 
 //Show user needed monthly contribution
 //   const Ppmt = parseFloat(
 //     PMT(rate, nper, pv, fv, type) - IPMT(pv, pmt, rate, nper)
 //   ).toFixed(2);
+function generateTable() {
+  // creates a <table> element and a <tbody> element
+  const tbl = document.createElement("table");
+  const tblBody = document.createElement("tbody");
+  const tblHeader = document.createElement("thead");
+  const row = document.createElement("tr");
+
+  tblHeader.innerHTML = `
+<tr>  
+<th>Years ${years}</th>
+<th>Contributing ${monthlySaved}</th>
+<th>Contributing ${ppmt} to get to goal</th> 
+</tr>`;
+
+  // creating all cells
+  for (let i = 0; i < 2; i++) {
+    // creates a table row
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < 2; j++) {
+      // Create a <td> element and a text node, make the text
+      // node the contents of the <td>, and put the <td> at
+      // the end of the table row
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+
+    // add the row to the end of the table body
+    tblBody.appendChild(row);
+  }
+  // put tblHeader into the table
+  tbl.appendChild(tblHeader);
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+  // appends <table> into <body>
+  document.body.appendChild(tbl);
+  // sets the border attribute of tbl to '2'
+  tbl.setAttribute("border", "2");
+}
