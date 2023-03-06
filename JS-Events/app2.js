8b0825c61ced42c3992890f818285d3a78931c69
AR123456 <coloradoz99@gmail.com>
2023-03-06 07:18:40 -0700

    on click version 2

---
+++ JS-Events/app2.js
@@ -0,0 +1,13 @@
+// refactor
+document.getElementById("colorChange").addEventListener("click", function () {
+  document.body.style.backgroundColor = randomColor();
+});
+randomColor = () => {
+  let characters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += characters[Math.floor(Math.random() * 16)];
+  }
+  console.log(color);
+  return color;
+};
