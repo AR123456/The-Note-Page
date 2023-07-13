ac21c694263ff10393eaab9a7bb90c13c087c1da
AR123456 <coloradoz99@gmail.com>
2023-07-13 06:26:12 -0600

    new way to write load more content

--- JS and CSS Endless Vertical Scroll/app.js
+++ JS and CSS Endless Vertical Scroll/app.js
@@ -1,10 +1,3 @@
-// const isScrollAtBottom = () => {
-//   const windowHeight = window.innerHeight;
-//   const bodyHeight = document.body.offsetHeight;
-//   const scrollPosition =
-//     window.scrollY || window.scrollX || document.documentElement.scrollTop;
-//   return scrollPosition > bodyHeight - windowHeight - 100;
-// };
 // a more efficient way to write the isScrollAtBottom function
 const isScrollAtBottom = () => {
   const { scrollY, scrollX } = window;
@@ -13,13 +6,20 @@ const isScrollAtBottom = () => {
   const { clientHeight, scrollHeight } = document.documentElement;
   return scrollPosition > scrollHeight - clientHeight - 100;
 };
-function loadMoreContent() {
-  setTimeout(function () {
+// function loadMoreContent() {
+//   setTimeout(function () {
+//     const newContent = document.createElement("div");
+//     newContent.innerHTML = "<p></p>";
+//     document.body.appendChild(newContent);
+//   }, 1000);
+// }
+const loadMoreContent = () => {
+  setTimeout(() => {
     const newContent = document.createElement("div");
     newContent.innerHTML = "<p></p>";
     document.body.appendChild(newContent);
   }, 1000);
-}
+};
 
 window.addEventListener("scroll", function () {
   if (isScrollAtBottom()) {
