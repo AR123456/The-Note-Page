91810cdd8057c043091a7680992139504858d4ac
AR123456 <coloradoz99@gmail.com>
2023-07-13 06:25:37 -0600

    changing over to es6

--- JS and CSS Endless Vertical Scroll/app.js
+++ JS and CSS Endless Vertical Scroll/app.js
@@ -1,10 +1,18 @@
-function isScrollAtBottom() {
-  const windowHeight = window.innerHeight;
-  const bodyHeight = document.body.offsetHeight;
+// const isScrollAtBottom = () => {
+//   const windowHeight = window.innerHeight;
+//   const bodyHeight = document.body.offsetHeight;
+//   const scrollPosition =
+//     window.scrollY || window.scrollX || document.documentElement.scrollTop;
+//   return scrollPosition > bodyHeight - windowHeight - 100;
+// };
+// a more efficient way to write the isScrollAtBottom function
+const isScrollAtBottom = () => {
+  const { scrollY, scrollX } = window;
   const scrollPosition =
-    window.scrollY || window.scrollX || document.documentElement.scrollTop;
-  return scrollPosition > bodyHeight - windowHeight - 100;
-}
+    scrollY || scrollX || document.documentElement.scrollTop;
+  const { clientHeight, scrollHeight } = document.documentElement;
+  return scrollPosition > scrollHeight - clientHeight - 100;
+};
 function loadMoreContent() {
   setTimeout(function () {
     const newContent = document.createElement("div");
