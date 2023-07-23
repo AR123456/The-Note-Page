64d77647c42264247adcbbef3477f19731d6cc3c
AR123456 <coloradoz99@gmail.com>
2023-07-23 17:17:38 -0600

    gsap left to right

--- CSS/GSAP Animations/Simple-GSAP-Animation/app.js
+++ CSS/GSAP Animations/Simple-GSAP-Animation/app.js
@@ -4,3 +4,5 @@ gsap.from(".header", { duration: 1, y: "-100%", ease: "bounce" });
 
 // annimate   links stagger animates each link in each .link in sequence
 gsap.from(".link", { duration: 2, opacity: 0, delay: 1, stagger: 0.5 });
+// go left to right
+gsap.from(".right", { duration: 2, x: "-100vw" });
