b0f12112b4b4269f907a8568eb0035ad6df3bd52
AR123456 <coloradoz99@gmail.com>
2023-07-23 17:17:00 -0600

    stagger

--- CSS/GSAP Animations/Simple-GSAP-Animation/app.js
+++ CSS/GSAP Animations/Simple-GSAP-Animation/app.js
@@ -1,3 +1,6 @@
 // treated like queryselector all
 // start the header above page and annimate in
-gsap.from(".header", { duration: 1, y: "-100%" });
+gsap.from(".header", { duration: 1, y: "-100%", ease: "bounce" });
+
+// annimate   links stagger animates each link in each .link in sequence
+gsap.from(".link", { duration: 2, opacity: 0, delay: 1, stagger: 0.5 });
