6ecaba2de87153d62d088f58062a42b4c0f400ee
AR123456 <coloradoz99@gmail.com>
2023-07-23 17:18:23 -0600

    footer animations

--- CSS/GSAP Animations/Simple-GSAP-Animation/app.js
+++ CSS/GSAP Animations/Simple-GSAP-Animation/app.js
@@ -7,4 +7,6 @@ gsap.from(".link", { duration: 2, opacity: 0, delay: 1, stagger: 0.5 });
 // go left to right
 gsap.from(".right", { duration: 2, x: "-100vw", delay: 1, ease: "power2.in" });
 // animate the left side bar in l r
-gsap.from(".left", { duration: 1, delay: 2, x: "-100%" });
+gsap.from(".left", { duration: 1, delay: 1.5, x: "-100%" });
+// animate the footer  go to a particular postiion
+gsap.to(".footer", { duration: 1, y: 0, ease: "elastic", delay: 3 });
