9af95a53fa43ca4d7ab682fb2a87b618e4dbec18
AR123456 <coloradoz99@gmail.com>
2023-07-23 17:18:00 -0600

    sidebars l to r

--- CSS/GSAP Animations/Simple-GSAP-Animation/app.js
+++ CSS/GSAP Animations/Simple-GSAP-Animation/app.js
@@ -5,4 +5,6 @@ gsap.from(".header", { duration: 1, y: "-100%", ease: "bounce" });
 // annimate   links stagger animates each link in each .link in sequence
 gsap.from(".link", { duration: 2, opacity: 0, delay: 1, stagger: 0.5 });
 // go left to right
-gsap.from(".right", { duration: 2, x: "-100vw" });
+gsap.from(".right", { duration: 2, x: "-100vw", delay: 1, ease: "power2.in" });
+// animate the left side bar in l r
+gsap.from(".left", { duration: 1, delay: 2, x: "-100%" });
