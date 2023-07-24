219573a7e1a84623d83df14123dd62279646da85
AR123456 <coloradoz99@gmail.com>
2023-07-24 06:34:19 -0600

    final working version gsap animations

--- CSS/GSAP Animations/Simple-GSAP-Animation/app.js
+++ CSS/GSAP Animations/Simple-GSAP-Animation/app.js
@@ -1,26 +1,26 @@
-// treated like queryselector all
-// // start the header above page and annimate in
-// gsap.from(".header", { duration: 1, y: "-100%", ease: "bounce" });
+// gsap.from('.header', { duration: 1, y: '-100%', ease: 'bounce' })
+// gsap.from('.link', { duration: 1, opacity: 0, delay: 1, stagger: .5 })
+// gsap.from('.right', { duration: 1, x: '-100vw', delay: 1, ease: 'power2.in' })
+// gsap.from('.left', { duration: 1, delay: 1.5, x: '-100%' })
+// gsap.to('.footer', { duration: 1, y: 0, ease: 'elastic', delay: 2.5 })
+// gsap.fromTo('.button', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, delay: 3.5, opacity: 1, scale: 1, rotation: 0 })
 
-// // annimate   links stagger animates each link in each .link in sequence
-// gsap.from(".link", { duration: 2, opacity: 0, delay: 1, stagger: 0.5 });
-// // go left to right
-// gsap.from(".right", { duration: 2, x: "-100vw", delay: 1, ease: "power2.in" });
-// // animate the left side bar in l r
-// gsap.from(".left", { duration: 1, delay: 1.5, x: "-100%" });
-// // animate the footer  go to a particular postiion
-// gsap.to(".footer", { duration: 1, y: 0, ease: "elastic", delay: 3 });
-// // the reverse button - spin in after other animations done
-// gsap.fromTo(
-//   ".button",
-//   { opacity: 0, scale: 0, rotation: 720 },
-//   { duration: 1, delay: 3.5, opacity: 1, scale: 1, rotation: 0 }
-// );
-// can take a js object
-// const obj = { x: 0 };
-// gsap.to(obj, { duration: 2, x: 100, onUpdate: () => console.log(obj.x) });
 const timeline = gsap.timeline({ defaults: { duration: 1 } });
 timeline
   .from(".header", { y: "-100%", ease: "bounce" })
   .from(".link", { opacity: 0, stagger: 0.5 })
-  .from(".right", { x: "-100vw", ease: "power2.in" });
+  .from(".right", { x: "-100vw", ease: "power2.in" }, 1)
+  .from(".left", { x: "-100%" }, "<.5")
+  .to(".footer", { y: 0, ease: "elastic" })
+  .fromTo(
+    ".button",
+    { opacity: 0, scale: 0, rotation: 720 },
+    { opacity: 1, scale: 1, rotation: 0 }
+  );
+
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+  timeline.timeScale(3);
+  timeline.reverse();
+});
