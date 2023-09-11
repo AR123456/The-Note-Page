4cacae4b4035c75f3069927872ed81366ace8f0b
AR123456 <coloradoz99@gmail.com>
2023-09-11 05:56:49 -0600

    on click not right

--- some simple JS just for paractice/Javascript Slide In Menu/app.js
+++ some simple JS just for paractice/Javascript Slide In Menu/app.js
@@ -1,3 +1,13 @@
-const menu = document.getElementsByClassName("menu");
+const menu = document.getElementById("menu");
+const x = document.getElementsByClassName("x");
+const expanded = document.getElementsByClassName("expanded");
 
 // on click of menu make menu-icon display none and .expanded display active
+// on click of the X in the selector makes expaned disply none and menu icon display active
+
+// use css to make the transion of above soothe
+
+menu.addEventListener("click", function () {
+  menu.display("none");
+  expanded.classList.display("active");
+});
