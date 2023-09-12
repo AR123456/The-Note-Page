00ba83c60dfd6ba83b3b60200fda883a91094d53
AR123456 <coloradoz99@gmail.com>
2023-09-12 06:45:53 -0600

    more attempts

--- some simple JS just for paractice/Javascript Slide In Menu/app.js
+++ some simple JS just for paractice/Javascript Slide In Menu/app.js
@@ -1,13 +1,33 @@
+// const menu = document.getElementById("menu");
+// const x = document.getElementById("x");
+// const expanded = document.getElementById("expanded");
+// const hamburger = document.getElementById("menu-icon");
+
+// hamburger.addEventListener("click", function () {
+//   hamburger.style.opacity = "0";
+//   expanded.style.opacity = "1";
+//   expanded.style.pointerEvents =
+//     "auto"; /* Enable pointer events for visible element */
+// });
+// x.addEventListener("click", function () {
+//   hamburger.style.opacity = "1";
+//   expanded.style.opacity = "0";
+//   expanded.style.pointerEvents =
+//     "none"; /* Disable pointer events for hidden element */
+// });
+
+// // burger menu - cannot use icon since it needs to animate from X to three lines back and forth.  The burger/x one or the other is always on the page
+// // likely cannot use visibility seems it needs to slide on and off page so likely css to move one into view and other out of view
+// // need to figure out how to transition that movement
 const menu = document.getElementById("menu");
 const x = document.getElementById("x");
 const expanded = document.getElementById("expanded");
 const hamburger = document.getElementById("menu-icon");
 
 hamburger.addEventListener("click", function () {
-  hamburger.style.visibility = "hidden";
-  expanded.style.visibility = "visible";
+  menu.classList.add("expanded"); // Add the "expanded" class
 });
+
 x.addEventListener("click", function () {
-  hamburger.style.visibility = "visible";
-  expanded.style.visibility = "hidden";
+  menu.classList.remove("expanded"); // Remove the "expanded" class
 });
