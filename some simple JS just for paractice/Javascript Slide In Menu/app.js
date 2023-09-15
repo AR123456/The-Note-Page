dc74438ef7d07a944666d27db3df1d498f1702eb
AR123456 <coloradoz99@gmail.com>
2023-09-15 06:06:26 -0600

    on click opacity

--- some simple JS just for paractice/Javascript Slide In Menu/app.js
+++ some simple JS just for paractice/Javascript Slide In Menu/app.js
@@ -1,5 +1,6 @@
 const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
 
 burger.addEventListener("click", () => {
-  alert("clicked");
+  nav.style.opacity = "1";
 });
