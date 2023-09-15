05401b442ad005903cade33eccfe1e5196589538
AR123456 <coloradoz99@gmail.com>
2023-09-15 06:06:57 -0600

    on click hide middle span

--- some simple JS just for paractice/Javascript Slide In Menu/app.js
+++ some simple JS just for paractice/Javascript Slide In Menu/app.js
@@ -1,6 +1,8 @@
 const burger = document.getElementById("burger");
 const nav = document.getElementById("nav");
+const spanInvisible = document.getElementById("span-invisible");
 
 burger.addEventListener("click", () => {
   nav.style.opacity = "1";
+  spanInvisible.style.opacity = 0;
 });
