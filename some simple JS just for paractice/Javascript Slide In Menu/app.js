dea484e9a5502c507cfdf5466640e5eb55e97274
AR123456 <coloradoz99@gmail.com>
2023-09-16 08:25:16 -0600

    position nav and burger

--- some simple JS just for paractice/Javascript Slide In Menu/app.js
+++ some simple JS just for paractice/Javascript Slide In Menu/app.js
@@ -4,5 +4,5 @@ const spanInvisible = document.getElementById("span-invisible");
 
 burger.addEventListener("click", () => {
   nav.style.opacity = "1";
-  spanInvisible.style.opacity = 0;
+  spanInvisible.style.opacity = "0";
 });
