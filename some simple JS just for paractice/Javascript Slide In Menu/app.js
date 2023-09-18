3b05b3356c0cf1a34e0c23d7d25a878ce1c26975
AR123456 <coloradoz99@gmail.com>
2023-09-18 06:47:39 -0600

    psudo code

--- some simple JS just for paractice/Javascript Slide In Menu/app.js
+++ some simple JS just for paractice/Javascript Slide In Menu/app.js
@@ -2,6 +2,9 @@ burger = document.querySelector(".burger");
 menu = document.querySelector("ul");
 
 burger.addEventListener("click", () => {
+  // on this click animate spans to x
+  // slide the menu in below the x
+  // if the X is clicked slide the menu back out
   console.log("clicked");
 });
 
