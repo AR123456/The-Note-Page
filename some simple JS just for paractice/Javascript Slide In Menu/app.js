1a94b8584be71380fd128b59af88038ad9234faa
AR123456 <coloradoz99@gmail.com>
2023-09-19 04:50:52 -0600

    toggle css on and off

--- some simple JS just for paractice/Javascript Slide In Menu/app.js
+++ some simple JS just for paractice/Javascript Slide In Menu/app.js
@@ -1,13 +1,8 @@
-burger = document.querySelector(".burger");
-menu = document.querySelector("ul");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
 
-burger.addEventListener("click", () => {
-  // on this click animate spans to x
-  // slide the menu in below the x
-  // if the X is clicked slide the menu back out
-  console.log("clicked");
-});
-
-menu.addEventListener("click", () => {
-  console.log("in unordered list");
+burger.addEventListener("click", (e) => {
+  // toggles the presence of the class "open" on the nav element
+  nav.classList.toggle("open");
+  burger.classList.toggle("open");
 });
