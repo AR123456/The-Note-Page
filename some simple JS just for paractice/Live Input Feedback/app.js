47ea45d11635e907931c38f21c7c0f8b9937e44e
AR123456 <coloradoz99@gmail.com>
2023-09-29 05:07:56 -0600

    array

--- some simple JS just for paractice/Live Input Feedback/app.js
+++ some simple JS just for paractice/Live Input Feedback/app.js
@@ -1,7 +1,10 @@
 const input = document.querySelector("input");
 // from chatGPT - verify
 const emailPattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$";
-
+const enteredArr = [];
 input.addEventListener("keyup", (e) => {
-  console.log(e.key);
+  keyPress = e.key;
+  enteredArr.push(keyPress);
+  console.log(keyPress);
+  console.log(enteredArr);
 });
