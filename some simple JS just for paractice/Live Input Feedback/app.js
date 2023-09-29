3f21f1756d0b0b4fc68f475b1bc56d70d4030647
AR123456 <coloradoz99@gmail.com>
2023-09-29 05:08:15 -0600

    better regex

--- some simple JS just for paractice/Live Input Feedback/app.js
+++ some simple JS just for paractice/Live Input Feedback/app.js
@@ -1,10 +1,16 @@
 const input = document.querySelector("input");
-// from chatGPT - verify
-const emailPattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$";
-const enteredArr = [];
+
 input.addEventListener("keyup", (e) => {
   keyPress = e.key;
   enteredArr.push(keyPress);
   console.log(keyPress);
   console.log(enteredArr);
 });
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
