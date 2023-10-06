d83582d2a2b5f37ec3a11569b20bf5d701ae9094
AR123456 <coloradoz99@gmail.com>
2023-10-06 05:35:53 -0600

    starting over

--- some simple JS just for paractice/Live Input Feedback/app.js
+++ some simple JS just for paractice/Live Input Feedback/app.js
@@ -1,28 +1,9 @@
 const input = document.querySelector("input");
-let email = "";
-input.addEventListener("keyup", (e) => {
-  keyPress = e.key;
-  if (keyPress != "Shift") {
-    email += keyPress;
-    validateEmail(email);
-  }
 
-  console.log(keyPress);
-  console.log(email);
-});
-
-const validateEmail = (email) => {
-  console.log(
-    String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
-  );
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
-console.log(String(email));
+// const validateEmail = (email) => {
+//   return String(email)
+//     .toLowerCase()
+//     .match(
+//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//     );
+// };
