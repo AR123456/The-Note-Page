3420e3cabd7d49c06a8af01ef39318d8824fb7b8
AR123456 <coloradoz99@gmail.com>
2023-10-08 08:33:42 -0600

    live input feedback

--- some simple JS just for paractice/Live Input Feedback/app.js
+++ some simple JS just for paractice/Live Input Feedback/app.js
@@ -1,14 +1,18 @@
+// get the input field for listener
 const emailInput = document.querySelector("#email");
 
 const emailRegex =
   /^([A-Za-z\d\.-]+)@([A-Za-z\d-]+)\.([A-Za-z]{2,6})(\.[A-Za-z]{2,6})?$/;
 
 emailInput.addEventListener("keyup", (e) => {
-  console.log(e.target.value);
-
+  // console.log(e.target.value);
+  // The test() method tests for a match in a string.If it finds a match, it returns true, otherwise it returns false.
+  //  https://www.w3schools.com/jsref/jsref_regexp_test.asp
   if (emailRegex.test(e.target.value)) {
+    // conditions to add valid class
     emailInput.parentElement.classList.add("valid");
   } else {
+    // remove valid class
     emailInput.parentElement.classList.remove("valid");
   }
 });
