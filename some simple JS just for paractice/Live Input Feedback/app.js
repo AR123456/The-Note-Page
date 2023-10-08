0a5f4f79d2f27fb704ee98cd0a70943db3f2ee0f
AR123456 <coloradoz99@gmail.com>
2023-10-08 08:32:50 -0600

    live input provided solution

--- some simple JS just for paractice/Live Input Feedback/app.js
+++ some simple JS just for paractice/Live Input Feedback/app.js
@@ -1,35 +1,18 @@
-const input = document.querySelector("input");
-const tickMark = document.querySelector(".tick");
-const formBox = document.querySelector("form input");
-//  onkeyup run this function
-input.addEventListener("keyup", validateEmail);
+const emailInput = document.querySelector("#email");
 
-function validateEmail() {
-  const emailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  // The value property sets or returns the value of the value attribute of a text field.The value property contains the default value OR the value a user types in (or a value set by a script).
-  const isValid = emailRegex.test(input.value);
+const emailRegex =
+  /^([A-Za-z\d\.-]+)@([A-Za-z\d-]+)\.([A-Za-z]{2,6})(\.[A-Za-z]{2,6})?$/;
 
-  if (isValid) {
-    // turn check mark green
-    tickMark.style.visibility = "visible";
-    tickMark.style.color = "green";
+emailInput.addEventListener("keyup", (e) => {
+  console.log(e.target.value);
 
-    formBox.style.border = "2px solid green";
-    formBox.style.outline = "2px solid green";
-
-    // border: 2px solid #777;
-    console.log("valid");
+  if (emailRegex.test(e.target.value)) {
+    emailInput.parentElement.classList.add("valid");
   } else {
-    tickMark.style.color = "red";
-    console.log("Invalid email address");
+    emailInput.parentElement.classList.remove("valid");
   }
-}
+});
 
-// const validateEmail = (email) => {
-//   return String(email)
-//     .toLowerCase()
-//     .match(
-//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//     );
-// };
+// better regex
+// const emailRegex =
+// /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
