64aad387a1b947a6e48ac0b159be5c7c3bd99fa7
AR123456 <coloradoz99@gmail.com>
2023-10-11 05:57:56 -0600

    working code

--- Fonts/Fun-Forest-Fonts/app.js
+++ Fonts/Fun-Forest-Fonts/app.js
@@ -1,11 +1,23 @@
-const FontButtons = document.querySelectorAll("font-buttons");
+// const FontButtons = document.querySelectorAll("font-buttons");
+// const TextWrapper = document.querySelector(".text-wrapper");
+// FontButtons.forEach((fontButton) => {
+//   //  get the font to display from the attribute
+//   const font = fontButton.getAttribute("data-font");
+//   fontButton.addEventListener("click", () => {
+//     console.log("click");
+//     TextWrapper.setAttribute("data-font", font);
+//     FontButtons.forEach((fButt) => fButt.classList.remove("active-button"));
+//     fontButton.classList.add("active-button");
+//   });
+// });
+const FontButtons = document.querySelectorAll(".font-buttons li");
 const TextWrapper = document.querySelector(".text-wrapper");
+
 FontButtons.forEach((fontButton) => {
-  //  get the font to display from the attribute
-  const font = fontButton.getAttribute("data-font");
+  const font = fontButton.getAttribute("Font");
   fontButton.addEventListener("click", () => {
     console.log("click");
-    TextWrapper.setAttribute("data-font", font);
+    TextWrapper.setAttribute("Font", font);
     FontButtons.forEach((fButt) => fButt.classList.remove("active-button"));
     fontButton.classList.add("active-button");
   });
