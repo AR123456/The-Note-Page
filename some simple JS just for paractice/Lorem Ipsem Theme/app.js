2d4d4767498e0496ceb1a35898417a9c04beaac9
AR123456 <coloradoz99@gmail.com>
2023-10-14 03:53:09 -0600

    starter code for change theme

---
+++ some simple JS just for paractice/Lorem Ipsem Theme/app.js
@@ -0,0 +1,23 @@
+// dom elements
+const buttons = document.querySelectorAll("button");
+const textarea = document.querySelector("textarea");
+const output = document.querySelector(".output");
+
+//  themes
+const themes = {
+  ninja: ["dojo", "shogun", "shinobi", "samuri", "shuriken"],
+  pokemon: ["pika", "pokeball", "pokedex", "evolve", "ash", "gym"],
+  space: ["universe", "galaxy", "telescope", "comet", "stars"],
+};
+
+// functions
+const themize = (inputText, theme) => {
+  // replace every 3rd word of input text with the words from the tehems array
+};
+
+const updateOutput = (text) => {};
+
+// event listener
+buttons.forEach((b) => {
+  b.addEventListener("click", (e) => {});
+});
