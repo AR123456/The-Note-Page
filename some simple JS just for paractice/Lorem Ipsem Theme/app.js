1ee7dc86930379126bef7c3b947a54c7d073c6c5
AR123456 <coloradoz99@gmail.com>
2023-10-15 07:23:47 -0600

    adding some notes about the theme problem

--- some simple JS just for paractice/Lorem Ipsem Theme/app.js
+++ some simple JS just for paractice/Lorem Ipsem Theme/app.js
@@ -5,35 +5,45 @@ const output = document.querySelector(".output");
 
 //  themes
 const themes = {
-  ninja: ["dojo", "shogun", "shinobi", "samuri", "shuriken"],
-  pokemon: ["pika", "pokeball", "pokedex", "evolve", "ash", "gym"],
+  flowers: ["Rose", "Tulip", "Sunflower", "Lily", "Daisy"],
+  fall: ["Pumpkin", "Autumn Leaves", "Sweater", "Apple", "Cider"],
   space: ["universe", "galaxy", "telescope", "comet", "stars"],
 };
 
 // functions
 const themize = (inputText, theme) => {
-  // replace every 3rd word of input text with the words from the tehems array
+  // make the input text into an array called textArray, based on space between the words
+
   const textArray = inputText.split(" ");
+  // iterate the textArray
   for (let i = 0; i < textArray.length; i++) {
+    // ind every 3rd word with modulus 
     if (i % 3 === 0) {
+      // get entry from array at random 
       const random = Math.floor(Math.random() * theme.length);
       textArray[i] = theme[random];
     }
   }
+  // turn the newly created array back into a string
   return textArray.join(" ");
 };
 
 const updateOutput = (text) => {
+  // inject it into the COM 
   output.textContent = text;
 };
 
-// event listener
+// event listener - determine what the data attribute is
 buttons.forEach((b) => {
   b.addEventListener("click", (e) => {
     console.log(e.target.dataset.theme);
+    // stop page from re loading
     e.preventDefault();
+    // check to see if there is text in the text area
     if (textarea.value) {
+      // which button was clicked on and what is its data attribute, dataset property
       const t = e.target.dataset.theme;
+      // call function that replaces the 3rd word - pass in the text area value and the theme
       const text = themize(textarea.value, themes[t]);
       updateOutput(text);
     }
