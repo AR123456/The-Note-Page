f14f5f06a1c616d273dc1e3e0395f11bb2b58e0d
AR123456 <coloradoz99@gmail.com>
2023-10-18 06:03:38 -0600

    commented final solution

--- some simple JS just for paractice/Flash Cards/app.js
+++ some simple JS just for paractice/Flash Cards/app.js
@@ -1,4 +1,7 @@
-var card = document.querySelector(".card");
-card.addEventListener("click", function () {
-  card.classList.toggle("is-flipped");
+const card = document.querySelector(".card");
+
+card.addEventListener("click", () => {
+  /* Although the classList property itself is read-only, you can modify its associated DOMTokenList using the add(), remove(), replace(), and toggle() methods. */
+  /* The toggle() method of the DOMTokenList interface removes an existing token from the list and returns false. If the token doesn't exist it's added and the function returns true. */
+  card.classList.toggle("flipped");
 });
