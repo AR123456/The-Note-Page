d2839a6f7b6a1001101cfceba1b30b11764049e5
AR123456 <coloradoz99@gmail.com>
2023-10-24 06:34:34 -0600

    adding slider values to dom

--- HTML/HTML Range Slider Water Wave/app.js
+++ HTML/HTML Range Slider Water Wave/app.js
@@ -1,2 +1,10 @@
-const slider = document.querySelector(".range");
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
+const range = document.getElementById("range");
+
+range.addEventListener("input", (e) => {
+  const value = +e.target.value;
+  //   get the label from the div
+  const label = e.target.nextElementSibling;
+  // put the value in the dom
+  label.innerHTML = value;
+});
