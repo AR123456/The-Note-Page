c3bfe2a7595c5c4138a8a6db2053e9e3bb9741bd
AR123456 <coloradoz99@gmail.com>
2023-10-25 05:30:34 -0600

    working with notes

--- HTML/HTML Range Slider Water Wave/app.js
+++ HTML/HTML Range Slider Water Wave/app.js
@@ -1,14 +1,3 @@
-// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range
-// const range = document.getElementById("range");
-
-// range.addEventListener("input", (e) => {
-//   const value = +e.target.value;
-//   //   get the label from the div
-//   const label = e.target.nextElementSibling;
-//   // put the value in the dom
-//   label.innerHTML = value;
-//   // use the scale function (see val-with-thumb in palooza ) for amount of fill in the label circle
-// });
 const input = document.querySelector("input");
 const globe = document.querySelector(".globe");
 const label = document.querySelector("label");
