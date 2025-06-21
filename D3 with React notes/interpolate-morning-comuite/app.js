1d04505c9684d9aea9563f058616ce3141603409
AR123456 <coloradoz99@gmail.com>
2025-06-21 05:18:05 -0400

    interpolation

---
+++ D3 with React notes/interpolate-morning-comuite/app.js
@@ -0,0 +1,14 @@
+let trainJourney = d3.interpolate(0, 69);
+console.log(trainJourney);
+let midpoint = trainJourney(0.5);
+console.log(midpoint);
+let almostHome = trainJourney(0.99);
+console.log(almostHome);
+let home = trainJourney(1);
+console.log(home);
+
+// And, if we look back at the docs for d3.scaleLinear, the definition of that function is almost exactly: "Constructs a new linear scale with the specified domain and range, the default interpolator, and clamping disabled."
+
+// I'm probably wrong and oversimplifying, but right now, it helps for me to think of scaling functions as interpolators with more bells and whistles. For example, since we now know that scaleLinear uses d3.interpolate under the hood (unless you tell it otherwise I guess), we can do something like this to get to get the same behaviour from a scale:
+
+// Object {midPointFromScale: 34.5}
