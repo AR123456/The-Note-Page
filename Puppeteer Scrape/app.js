d4bb20dcd108658df2f613fbd851a7da7f617589
AR123456 <coloradoz99@gmail.com>
2025-10-07 05:55:22 -0400

    notes on example

--- Puppeteer Scrape/app.js
+++ Puppeteer Scrape/app.js
@@ -3,7 +3,8 @@ const puppeteer = require("puppeteer");
 
 (async () => {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  //   const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
