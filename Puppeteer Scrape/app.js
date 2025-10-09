2877a385ad396b55e577a44844ba88e25591b897
AR123456 <coloradoz99@gmail.com>
2025-10-09 09:19:36 -0400

    eval to get title

--- Puppeteer Scrape/app.js
+++ Puppeteer Scrape/app.js
@@ -9,11 +9,15 @@ async function run() {
   // then the page to go to
   await page.goto("https://www.traversymedia.com");
   // take a screen shot of the page
-  await page.screenshot({ path: "example.png", fullPage: true });
+  // await page.screenshot({ path: "example.png", fullPage: true });
   // making a pdf
-  await page.pdf({ path: "example.pdf", format: "A4" });
+  // await page.pdf({ path: "example.pdf", format: "A4" });
   // access dom elements ect
-
+  // get all the HTML of the whole site  and console.log
+  // const html = await page.content();
+  // to get elements use page.evaluate which is a higher order function
+  const title = await page.evaluate(() => document.title);
+  console.log(title);
   // close the browser
   await browser.close();
 }
